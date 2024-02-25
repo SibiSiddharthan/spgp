1671670d7c1e7902bcd86d95fa2ca07271f6a30f
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-02-25 22:16:44 +0530

    Add CRC-24 implementation

---
+++ src/core/crc24.h
@@ -0,0 +1,25 @@
+#ifndef SPGP_CRC24_H
+#define SPGP_CRC24_H
+
+#include "macros.h"
+
+/*
+   Name   : "CRC-24"
+   Width  : 24
+   Poly   : 0x864CFB
+   Init   : 0xB704CE
+   RefIn  : False
+   RefOut : False
+   XorOut : 0xFFFFFF
+   Check  : 0x21CF02
+*/
+
+#define CRC24_INIT  0xB704CE
+#define CRC24_POLY  0x864CFB
+#define CRC24_FINAL 0xFFFFFF
+
+uint32_t crc24_init(void);
+uint32_t crc24_update(uint32_t crc, const byte_t *data, size_t size);
+uint32_t crc24_final(uint32_t crc);
+
+#endif
