b764b13b0a5b86d38c0d732b4cde3eaff916eba6
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-04-04 23:21:20 +0530

    Add twofish.h

---
+++ src/crypto/include/twofish.h
@@ -0,0 +1,37 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef CRYPTO_TWOFISH_H
+#define CRYPTO_TWOFISH_H
+
+#include <types.h>
+
+#define TWOFISH_ROUNDS 16
+#define TWOFISH_BLOCK_SIZE 16
+
+typedef uint8_t twofish_round_key[16];
+
+typedef enum _twofish_type
+{
+	TWOFISH128,
+	TWOFISH192,
+	TWOFISH256
+} twofish_type;
+
+typedef struct _twofish_key
+{
+	twofish_type type;
+	twofish_round_key encryption_round_key[TWOFISH_ROUNDS + 1];
+} twofish_key;
+
+twofish_key *new_twofish_key(twofish_type type, byte_t *key);
+void delete_twofish_key(twofish_key *key);
+
+void twofish_encrypt_block(twofish_key *key, byte_t plaintext[TWOFISH_BLOCK_SIZE], byte_t ciphertext[TWOFISH_BLOCK_SIZE]);
+void twofish_decrypt_block(twofish_key *key, byte_t ciphertext[TWOFISH_BLOCK_SIZE], byte_t plaintext[TWOFISH_BLOCK_SIZE]);
+
+#endif
