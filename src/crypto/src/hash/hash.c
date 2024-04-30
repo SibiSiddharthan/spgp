06c94b54298214921629334ad2ae4ecb6b72fe08
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-05-01 00:22:02 +0530

    Add blake2 reset shortcut to hash wrapper

--- src/crypto/src/hash/hash.c
+++ src/crypto/src/hash/hash.c
@@ -30,125 +30,155 @@ hash_ctx *hash_ctx_new(hash_algorithm algorithm)
 	switch (algorithm)
 	{
 	case MD5:
+	{
 		hash_size = 16;
 		_ctx = md5_init();
 		_free = (void (*)(void *))md5_free;
 		_reset = (void (*)(void *))md5_reset;
 		_update = (void (*)(void *, void *, size_t))md5_update;
 		_final = (void (*)(void *, byte_t *))md5_final;
-		break;
+	}
+	break;
 	case RIPEMD160:
+	{
 		hash_size = 20;
 		_ctx = ripemd160_init();
 		_free = (void (*)(void *))ripemd160_free;
 		_reset = (void (*)(void *))ripemd160_reset;
 		_update = (void (*)(void *, void *, size_t))ripemd160_update;
 		_final = (void (*)(void *, byte_t *))ripemd160_final;
-		break;
+	}
+	break;
 	case BLAKE2B:
+	{
 		blake2b_param b2bp = {.digest_length = 64, .key_length = 0, .depth = 1, .fanout = 1};
 		hash_size = 64;
 		_ctx = blake2b_init(&b2bp, NULL);
 		_free = (void (*)(void *))blake2b_free;
 		_update = (void (*)(void *, void *, size_t))blake2b_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))blake2b_final;
-		break;
+	}
+	break;
 	case BLAKE2S:
+	{
 		blake2s_param b2sp = {.digest_length = 32, .key_length = 0, .depth = 1, .fanout = 1};
 		hash_size = 32;
 		_ctx = blake2s_init(&b2sp, NULL);
 		_free = (void (*)(void *))blake2s_free;
 		_update = (void (*)(void *, void *, size_t))blake2s_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))blake2s_final;
-		break;
+	}
+	break;
 	case SHA1:
+	{
 		hash_size = 20;
 		_ctx = sha1_init();
 		_free = (void (*)(void *))sha1_free;
 		_reset = (void (*)(void *))sha1_reset;
 		_update = (void (*)(void *, void *, size_t))sha1_update;
 		_final = (void (*)(void *, byte_t *))sha1_final;
-		break;
+	}
+	break;
 	case SHA224:
+	{
 		hash_size = 28;
 		_ctx = sha224_init();
 		_free = (void (*)(void *))sha224_free;
 		_reset = (void (*)(void *))sha224_reset;
 		_update = (void (*)(void *, void *, size_t))sha224_update;
 		_final = (void (*)(void *, byte_t *))sha224_final;
-		break;
+	}
+	break;
 	case SHA256:
+	{
 		hash_size = 32;
 		_ctx = sha256_init();
 		_free = (void (*)(void *))sha256_free;
 		_reset = (void (*)(void *))sha256_reset;
 		_update = (void (*)(void *, void *, size_t))sha256_update;
 		_final = (void (*)(void *, byte_t *))sha256_final;
-		break;
+	}
+	break;
 	case SHA384:
+	{
 		hash_size = 48;
 		_ctx = sha384_init();
 		_free = (void (*)(void *))sha384_free;
 		_reset = (void (*)(void *))sha384_reset;
 		_update = (void (*)(void *, void *, size_t))sha384_update;
 		_final = (void (*)(void *, byte_t *))sha384_final;
-		break;
+	}
+	break;
 	case SHA512:
+	{
 		hash_size = 64;
 		_ctx = sha512_init();
 		_free = (void (*)(void *))sha512_free;
 		_reset = (void (*)(void *))sha512_reset;
 		_update = (void (*)(void *, void *, size_t))sha512_update;
 		_final = (void (*)(void *, byte_t *))sha512_final;
-		break;
+	}
+	break;
 	case SHA512_224:
+	{
 		hash_size = 28;
 		_ctx = sha512_224_init();
 		_free = (void (*)(void *))sha512_224_free;
 		_reset = (void (*)(void *))sha512_224_reset;
 		_update = (void (*)(void *, void *, size_t))sha512_224_update;
 		_final = (void (*)(void *, byte_t *))sha512_224_final;
-		break;
+	}
+	break;
 	case SHA512_256:
+	{
 		hash_size = 32;
 		_ctx = sha512_256_init();
 		_free = (void (*)(void *))sha512_256_free;
 		_reset = (void (*)(void *))sha512_256_reset;
 		_update = (void (*)(void *, void *, size_t))sha512_256_update;
 		_final = (void (*)(void *, byte_t *))sha512_256_final;
-		break;
+	}
+	break;
 	case SHA3_224:
+	{
 		hash_size = 28;
 		_ctx = sha3_init(224);
 		_free = (void (*)(void *))sha3_free;
 		_reset = (void (*)(void *))sha3_reset;
 		_update = (void (*)(void *, void *, size_t))sha3_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))sha3_final;
-		break;
+	}
+	break;
 	case SHA3_256:
+	{
 		hash_size = 32;
 		_ctx = sha3_init(224);
 		_free = (void (*)(void *))sha3_free;
 		_reset = (void (*)(void *))sha3_reset;
 		_update = (void (*)(void *, void *, size_t))sha3_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))sha3_final;
-		break;
+	}
+	break;
 	case SHA3_384:
+	{
 		hash_size = 48;
 		_ctx = sha3_init(224);
 		_free = (void (*)(void *))sha3_free;
 		_reset = (void (*)(void *))sha3_reset;
 		_update = (void (*)(void *, void *, size_t))sha3_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))sha3_final;
-		break;
+	}
+	break;
 	case SHA3_512:
+	{
 		hash_size = 64;
 		_ctx = sha3_init(224);
 		_free = (void (*)(void *))sha3_free;
 		_reset = (void (*)(void *))sha3_reset;
 		_update = (void (*)(void *, void *, size_t))sha3_update;
 		_final_size = (void (*)(void *, byte_t *, size_t))sha3_final;
-		break;
+	}
+	break;
 	default:
 		return NULL;
 	}
@@ -167,6 +197,7 @@ hash_ctx *hash_ctx_new(hash_algorithm algorithm)
 
 	memset(hctx, 0, sizeof(hash_ctx));
 
+	hctx->algorithm = algorithm;
 	hctx->hash_size = hash_size;
 	hctx->max_input_size = max_input_size;
 
@@ -191,6 +222,21 @@ void hash_ctx_reset(hash_ctx *hctx)
 	if (hctx->_reset != NULL)
 	{
 		hctx->_reset(hctx->_ctx);
+		return;
+	}
+
+	// For Blake2
+	if (hctx->algorithm == BLAKE2B)
+	{
+		blake2b_param b2bp = {.digest_length = 64, .key_length = 0, .depth = 1, .fanout = 1};
+		blake2b_reset(hctx->_ctx, &b2bp, NULL);
+		return;
+	}
+	if (hctx->algorithm == BLAKE2S)
+	{
+		blake2s_param b2sp = {.digest_length = 32, .key_length = 0, .depth = 1, .fanout = 1};
+		blake2s_reset(hctx->_ctx, &b2sp, NULL);
+		return;
 	}
 }
 
