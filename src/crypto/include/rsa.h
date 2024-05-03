55bfae1286cf645b9fc2a434f5b46da8c0482a9d
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-05-03 20:26:12 +0530

    rsa.h add function definitions

--- src/crypto/include/rsa.h
+++ src/crypto/include/rsa.h
@@ -32,6 +32,21 @@ typedef struct _oaep_options
 	mgf *mask;
 } oaep_options;
 
+typedef struct _rsa_pss_ctx
+{
+	rsa_key *key;
+	hash_ctx *hctx;
+	mgf *mask;
+	size_t size;
+} rsa_pss_ctx;
+
+typedef struct _rsa_pkcs_ctx
+{
+	rsa_key *key;
+	hash_ctx *hctx;
+	size_t size;
+} rsa_pkcs_ctx;
+
 typedef struct _rsa_signature
 {
 	bignum_t sign;
@@ -49,10 +64,35 @@ bignum_t *rsa_private_decrypt(rsa_key *key, bignum_t *cipher);
 int32_t rsa_encrypt_oaep(rsa_key *key, buffer_t *plaintext, buffer_t *label, buffer_t *ciphertext, oaep_options *options);
 int32_t rsa_decrypt_oaep(rsa_key *key, buffer_t *ciphertext, buffer_t *label, buffer_t *plaintext, oaep_options *options);
 
-rsa_signature *rsa_sign_pkcs(rsa_key *key, byte_t *message, size_t size);
-int32_t rsa_verify_pkcs(rsa_key *key, rsa_signature *signature, byte_t *message, size_t size);
+int32_t rsa_encrypt_pkcs(rsa_key *key, buffer_t *plaintext, buffer_t *ciphertext);
+int32_t rsa_decrypt_pkcs(rsa_key *key, buffer_t *ciphertext, buffer_t *plaintext);
 
-rsa_signature *rsa_sign_pss(rsa_key *key, byte_t *message, size_t size);
-int32_t rsa_verify_pss(rsa_key *key, rsa_signature *signature, byte_t *message, size_t size);
+rsa_pss_ctx *rsa_sign_pss_init(rsa_key *key, hash_ctx *hctx, mgf *mask);
+void rsa_sign_pss_free(rsa_pss_ctx *rctx);
+void rsa_sign_pss_reset(rsa_pss_ctx *rctx, rsa_key *key, hash_ctx *hctx, mgf *mask);
+void rsa_sign_pss_update(rsa_pss_ctx *rctx, void *message, size_t size);
+rsa_signature *rsa_sign_pss_final(rsa_pss_ctx *rctx);
+rsa_signature *rsa_sign_pss(rsa_key *key, hash_ctx *hctx, mgf *mask, void *message, size_t size);
+
+rsa_pss_ctx *rsa_verify_pss_init(rsa_key *key, hash_ctx *hctx, mgf *mask);
+void rsa_verify_pss_free(rsa_pss_ctx *rctx);
+void rsa_verify_pss_reset(rsa_pss_ctx *rctx, rsa_key *key, hash_ctx *hctx, mgf *mask);
+void rsa_verify_pss_update(rsa_pss_ctx *rctx, void *message, size_t size);
+int32_t rsa_verify_pss_final(rsa_pss_ctx *rctx, rsa_signature *rsign);
+int32_t *rsa_verify_pss(rsa_key *key, hash_ctx *hctx, mgf *mask, void *message, size_t size, rsa_signature *rsign);
+
+rsa_pkcs_ctx *rsa_sign_pkcs_init(rsa_key *key, hash_ctx *hctx);
+void rsa_sign_pkcs_free(rsa_pkcs_ctx *rctx);
+void rsa_sign_pkcs_reset(rsa_pkcs_ctx *rctx, rsa_key *key, hash_ctx *hctx);
+void rsa_sign_pkcs_update(rsa_pkcs_ctx *rctx, void *message, size_t size);
+rsa_signature *rsa_sign_pkcs_final(rsa_pkcs_ctx *rctx);
+rsa_signature *rsa_sign_pkcs(rsa_key *key, hash_ctx *hctx, void *message, size_t size);
+
+rsa_pkcs_ctx *rsa_verify_pkcs_init(rsa_key *key, hash_ctx *hctx);
+void rsa_verify_pkcs_free(rsa_pkcs_ctx *rctx);
+void rsa_verify_pkcs_reset(rsa_pkcs_ctx *rctx, rsa_key *key, hash_ctx *hctx);
+void rsa_verify_pkcs_update(rsa_pkcs_ctx *rctx, void *message, size_t size);
+int32_t rsa_verify_pkcs_final(rsa_pkcs_ctx *rctx, rsa_signature *rsign);
+int32_t *rsa_verify_pkcs(rsa_key *key, hash_ctx *hctx, void *message, size_t size, rsa_signature *rsign);
 
 #endif
