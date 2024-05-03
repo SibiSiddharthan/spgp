ff84f0a54ac25d546ee6d72487ab5fd868c0451e
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-05-04 00:15:07 +0530

    Update rsa.h

--- src/crypto/include/rsa.h
+++ src/crypto/include/rsa.h
@@ -37,14 +37,12 @@ typedef struct _rsa_pss_ctx
 	rsa_key *key;
 	hash_ctx *hctx;
 	mgf *mask;
-	size_t size;
 } rsa_pss_ctx;
 
 typedef struct _rsa_pkcs_ctx
 {
 	rsa_key *key;
 	hash_ctx *hctx;
-	size_t size;
 } rsa_pkcs_ctx;
 
 typedef struct _rsa_signature
@@ -78,8 +76,8 @@ rsa_pss_ctx *rsa_verify_pss_init(rsa_key *key, hash_ctx *hctx, mgf *mask);
 void rsa_verify_pss_free(rsa_pss_ctx *rctx);
 void rsa_verify_pss_reset(rsa_pss_ctx *rctx, rsa_key *key, hash_ctx *hctx, mgf *mask);
 void rsa_verify_pss_update(rsa_pss_ctx *rctx, void *message, size_t size);
-int32_t rsa_verify_pss_final(rsa_pss_ctx *rctx, rsa_signature *rsign);
-int32_t *rsa_verify_pss(rsa_key *key, hash_ctx *hctx, mgf *mask, void *message, size_t size, rsa_signature *rsign);
+int32_t rsa_verify_pss_final(rsa_pss_ctx *rctx, rsa_signature *rsign, rsa_signature *expected);
+int32_t rsa_verify_pss(rsa_key *key, hash_ctx *hctx, mgf *mask, void *message, size_t size, rsa_signature *rsign, rsa_signature *expected);
 
 rsa_pkcs_ctx *rsa_sign_pkcs_init(rsa_key *key, hash_ctx *hctx);
 void rsa_sign_pkcs_free(rsa_pkcs_ctx *rctx);
@@ -92,7 +90,7 @@ rsa_pkcs_ctx *rsa_verify_pkcs_init(rsa_key *key, hash_ctx *hctx);
 void rsa_verify_pkcs_free(rsa_pkcs_ctx *rctx);
 void rsa_verify_pkcs_reset(rsa_pkcs_ctx *rctx, rsa_key *key, hash_ctx *hctx);
 void rsa_verify_pkcs_update(rsa_pkcs_ctx *rctx, void *message, size_t size);
-int32_t rsa_verify_pkcs_final(rsa_pkcs_ctx *rctx, rsa_signature *rsign);
-int32_t *rsa_verify_pkcs(rsa_key *key, hash_ctx *hctx, void *message, size_t size, rsa_signature *rsign);
+int32_t rsa_verify_pkcs_final(rsa_pkcs_ctx *rctx, rsa_signature *rsign, rsa_signature *expected);
+int32_t rsa_verify_pkcs(rsa_key *key, hash_ctx *hctx, void *message, size_t size, rsa_signature *rsign, rsa_signature *expected);
 
 #endif
