de2313785294537816c951339c69992072f12996
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-05-20 00:04:44 +0530

    Add round.h

---
+++ src/crypto/include/round.h
@@ -0,0 +1,14 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef CRYPTO_ROUND_H
+#define CRYPTO_ROUND_H
+
+#define ROUND_UP(x, y)   ((((x) + ((y) - 1)) / (y)) * (y))
+#define ROUND_DOWN(x, y) (((x) / (y)) * (y))
+
+#endif
