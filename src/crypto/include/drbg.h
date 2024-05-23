3af9f533bea01f02c0ad8b59fe863b2be0bddb6b
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-05-23 23:36:12 +0530

    Implement HMAC-DRBG

--- src/crypto/include/drbg.h
+++ src/crypto/include/drbg.h
@@ -9,9 +9,12 @@
 #define CRYPTO_DRBG_H
 
 #include <types.h>
+#include <cipher.h>
 #include <hash.h>
+#include <hmac.h>
 
 #define MAX_SEED_SIZE             128
+#define MAX_KEY_SIZE              128
 #define MAX_PERSONALIZATION_SIZE  (1ull << 32)
 #define MAX_ADDITIONAL_INPUT_SIZE (1ull << 32)
 #define MAX_DRBG_OUTPUT_SIZE      (1ull << 16)
@@ -32,7 +35,14 @@ typedef struct _hash_drbg
 
 typedef struct _hmac_drbg
 {
-	void *x;
+	hmac_ctx *hctx;
+	size_t drbg_size;
+	byte_t key[MAX_KEY_SIZE];
+	byte_t seed[MAX_SEED_SIZE];
+	uint16_t output_size;
+	uint16_t security_strength;
+	uint64_t reseed_interval;
+	uint64_t reseed_counter;
 } hmac_drbg;
 
 typedef struct _ctr_drbg
@@ -69,10 +79,12 @@ void hash_drbg_delete(hash_drbg *hdrbg);
 int32_t hash_drbg_reseed(hash_drbg *hdrbg, byte_t *additional_input, size_t input_size);
 int32_t hash_drbg_generate(hash_drbg *hdrbg, byte_t *additional_input, size_t input_size, void *output, size_t output_size);
 
-hmac_drbg *hmac_drbg_init(void);
-void hmac_drbg_free(hmac_drbg *drbg);
-void hmac_drbg_reseed(hmac_drbg *drbg);
-void hmac_drbg_generate(hmac_drbg *drbg, void *buffer, size_t size);
+hmac_drbg *hmac_drbg_init(void *ptr, size_t size, hmac_algorithm algorithm, uint32_t reseed_interval, byte_t *personalization,
+						  size_t personalization_size);
+hmac_drbg *hmac_drbg_new(hmac_algorithm algorithm, uint32_t reseed_interval, byte_t *personalization, size_t personalization_size);
+void hmac_drbg_delete(hmac_drbg *hdrbg);
+int32_t hmac_drbg_reseed(hmac_drbg *hdrbg, byte_t *additional_input, size_t input_size);
+int32_t hmac_drbg_generate(hmac_drbg *hdrbg, byte_t *additional_input, size_t input_size, void *output, size_t output_size);
 
 ctr_drbg *ctr_drbg_init(void);
 void ctr_drbg_free(ctr_drbg *drbg);
---
+++ src/crypto/src/rand/hmac-drbg.c
@@ -0,0 +1,358 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <drbg.h>
+#include <round.h>
+#include <hmac.h>
+#include <sha.h>
+
+int32_t get_entropy(byte_t *buffer, size_t size);
+
+static inline size_t get_approved_hash_ctx_size(hmac_algorithm algorithm)
+{
+	switch (algorithm)
+	{
+	case HMAC_SHA1:
+		return sizeof(sha1_ctx);
+	case HMAC_SHA224:
+	case HMAC_SHA256:
+		return sizeof(sha256_ctx);
+	case HMAC_SHA384:
+	case HMAC_SHA512:
+	case HMAC_SHA512_224:
+	case HMAC_SHA512_256:
+		return sizeof(sha512_ctx);
+	default:
+		return 0;
+	}
+}
+
+static void hmac_drbg_update(hmac_drbg *hdrbg, byte_t *provided, size_t provided_size)
+{
+	hmac_ctx *hctx = hdrbg->hctx;
+	byte_t *key = hdrbg->key;
+	byte_t *seed = hdrbg->seed;
+
+	size_t output_size = hctx->hash_size;
+	byte_t byte;
+
+	if (provided == NULL)
+	{
+		byte = 0x00;
+
+		hmac_update(hctx, seed, output_size);
+		hmac_update(hctx, &byte, 1);
+		hmac_final(hctx, key, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		hmac_update(hctx, seed, output_size);
+		hmac_final(hctx, seed, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		return;
+	}
+
+	else
+	{
+		byte = 0x00;
+
+		hmac_update(hctx, seed, output_size);
+		hmac_update(hctx, &byte, 1);
+		hmac_update(hctx, provided, provided_size);
+		hmac_final(hctx, key, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		hmac_update(hctx, seed, output_size);
+		hmac_final(hctx, seed, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		byte = 0x01;
+
+		hmac_update(hctx, seed, output_size);
+		hmac_update(hctx, &byte, 1);
+		hmac_update(hctx, provided, provided_size);
+		hmac_final(hctx, key, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		hmac_update(hctx, seed, output_size);
+		hmac_final(hctx, seed, output_size);
+
+		hmac_reset(hctx, key, output_size);
+
+		return;
+	}
+}
+
+static int32_t hmac_drbg_init_state(hmac_drbg *hdrbg, size_t output_size, hmac_algorithm algorithm, byte_t *personalization,
+									size_t personalization_size)
+{
+	int32_t status = -1;
+	size_t security = ROUND_UP(hdrbg->security_strength / 8, 8);
+	size_t seed_material_size = (2 * security) + personalization_size;
+
+	byte_t *seed_material = NULL;
+
+	seed_material = (byte_t *)malloc(seed_material_size);
+
+	if (seed_material == NULL)
+	{
+		return status;
+	}
+
+	// Entropy and Nonce
+	status = get_entropy(seed_material, 2 * security);
+
+	if (status != 0)
+	{
+		goto end;
+	}
+
+	if (personalization != NULL)
+	{
+		memcpy(seed_material + (2 * security), personalization, personalization_size);
+	}
+
+	memset(hdrbg->key, 0x00, output_size);
+	memset(hdrbg->seed, 0x01, output_size);
+
+	hmac_init(hdrbg->hctx, hdrbg->drbg_size - sizeof(hmac_drbg), algorithm, hdrbg->key, output_size);
+	hmac_drbg_update(hdrbg, seed_material, seed_material_size);
+
+	hdrbg->reseed_counter = 1;
+
+	status = 0;
+
+end:
+	free(seed_material);
+	return status;
+}
+
+static hmac_drbg *hmac_drbg_init_checked(void *ptr, size_t ctx_size, hmac_algorithm algorithm, uint32_t reseed_interval,
+										 byte_t *personalization, size_t personalization_size)
+{
+	hmac_drbg *hdrbg = (hmac_drbg *)ptr;
+
+	uint16_t security_strength;
+	uint16_t output_size;
+
+	switch (algorithm)
+	{
+	case HMAC_SHA1:
+		security_strength = 160;
+		output_size = SHA1_HASH_SIZE;
+		break;
+
+	case HMAC_SHA224:
+	case HMAC_SHA512_224:
+		security_strength = 224;
+		output_size = SHA224_HASH_SIZE;
+		break;
+
+	case HMAC_SHA256:
+	case HMAC_SHA512_256:
+		security_strength = 256;
+		output_size = SHA256_HASH_SIZE;
+		break;
+
+	case HMAC_SHA384:
+		security_strength = 384;
+		output_size = SHA384_HASH_SIZE;
+		break;
+
+	case HMAC_SHA512:
+		security_strength = 512;
+		output_size = SHA512_HASH_SIZE;
+		break;
+	default: // Prevent -Wswitch
+		return NULL;
+	}
+
+	memset(hdrbg, 0, sizeof(hmac_drbg));
+
+	hdrbg->hctx = (hmac_ctx *)((byte_t *)hdrbg + sizeof(hmac_drbg));
+	hdrbg->drbg_size = sizeof(hmac_drbg) + sizeof(hmac_ctx) + ctx_size;
+	hdrbg->reseed_interval = reseed_interval;
+	hdrbg->output_size = output_size;
+	hdrbg->security_strength = security_strength;
+
+	if (hmac_drbg_init_state(hdrbg, output_size, algorithm, personalization, personalization_size) != 0)
+	{
+		memset(hdrbg, 0, hdrbg->drbg_size);
+		return NULL;
+	}
+
+	return hdrbg;
+}
+
+hmac_drbg *hmac_drbg_init(void *ptr, size_t size, hmac_algorithm algorithm, uint32_t reseed_interval, byte_t *personalization,
+						  size_t personalization_size)
+{
+
+	size_t ctx_size = get_approved_hash_ctx_size(algorithm);
+	size_t required_size = sizeof(hmac_drbg) + sizeof(hmac_ctx) + ctx_size;
+
+	if (ctx_size == 0)
+	{
+		return NULL;
+	}
+
+	if (size < required_size)
+	{
+		return NULL;
+	}
+
+	if (personalization_size > MAX_PERSONALIZATION_SIZE)
+	{
+		return NULL;
+	}
+
+	return hmac_drbg_init_checked(ptr, ctx_size, algorithm, reseed_interval, personalization, personalization_size);
+}
+
+hmac_drbg *hmac_drbg_new(hmac_algorithm algorithm, uint32_t reseed_interval, byte_t *personalization, size_t personalization_size)
+{
+	hmac_drbg *hdrbg = NULL;
+	hmac_drbg *result = NULL;
+
+	size_t ctx_size = get_approved_hash_ctx_size(algorithm);
+	size_t required_size = sizeof(hmac_drbg) + sizeof(hmac_ctx) + ctx_size;
+
+	if (ctx_size == 0)
+	{
+		return NULL;
+	}
+
+	if (personalization_size > MAX_PERSONALIZATION_SIZE)
+	{
+		return NULL;
+	}
+
+	hdrbg = (hmac_drbg *)malloc(required_size);
+
+	if (hdrbg == NULL)
+	{
+		return NULL;
+	}
+
+	result = hmac_drbg_init_checked(hdrbg, ctx_size, algorithm, reseed_interval, personalization, personalization_size);
+
+	if (result == NULL)
+	{
+		free(hdrbg);
+		return NULL;
+	}
+
+	return result;
+}
+
+void hmac_drbg_delete(hmac_drbg *hdrbg)
+{
+	memset(hdrbg, 0, hdrbg->drbg_size);
+	free(hdrbg);
+}
+
+int32_t hmac_drbg_reseed(hmac_drbg *hdrbg, byte_t *additional_input, size_t input_size)
+{
+	int32_t status = -1;
+	size_t security = ROUND_UP(hdrbg->security_strength / 8, 8);
+	size_t seed_material_size = security + input_size;
+
+	byte_t *seed_material = NULL;
+
+	seed_material = (byte_t *)malloc(seed_material_size);
+
+	if (seed_material == NULL)
+	{
+		return -1;
+	}
+
+	status = get_entropy(seed_material, security);
+
+	if (status != 0)
+	{
+		free(seed_material);
+		return -1;
+	}
+
+	if (additional_input != NULL && input_size > 0)
+	{
+		memcpy(seed_material + security, additional_input, input_size);
+	}
+
+	hmac_drbg_update(hdrbg, seed_material, seed_material_size);
+	hdrbg->reseed_counter = 1;
+
+	free(seed_material);
+	return 0;
+}
+
+int32_t hmac_drbg_generate(hmac_drbg *hdrbg, byte_t *additional_input, size_t input_size, void *output, size_t output_size)
+{
+	int32_t status = 0;
+	hmac_ctx *hctx = hdrbg->hctx;
+
+	size_t count = 0;
+	size_t hmac_size = hdrbg->output_size;
+
+	// Check buffer
+	if (output == NULL)
+	{
+		return -1;
+	}
+
+	// Check requested size
+	if (output_size > MAX_DRBG_OUTPUT_SIZE)
+	{
+		return -1;
+	}
+
+	// Reseed
+	if (hdrbg->reseed_counter == hdrbg->reseed_interval)
+	{
+		status = hmac_drbg_reseed(hdrbg, NULL, 0);
+
+		if (status == -1)
+		{
+			return -1;
+		}
+	}
+
+	if (additional_input != NULL && input_size > 0)
+	{
+		hmac_drbg_update(hdrbg, additional_input, input_size);
+	}
+
+	while ((count + hmac_size) <= output_size)
+	{
+		hmac_update(hctx, hdrbg->seed, hmac_size);
+		hmac_final(hctx, output, hmac_size);
+
+		hmac_reset(hctx, hdrbg->key, hmac_size);
+		count += hmac_size;
+	}
+
+	if ((output_size - count) != 0)
+	{
+		hmac_update(hctx, hdrbg->seed, hmac_size);
+		hmac_final(hctx, output, output_size - count); // Automatic truncation
+
+		hmac_reset(hctx, hdrbg->key, hmac_size);
+	}
+
+	hmac_drbg_update(hdrbg, additional_input, input_size);
+	hdrbg->reseed_counter++;
+
+	return 0;
+}
