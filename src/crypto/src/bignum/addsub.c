b82fff13be421be9c5fe2c5aeddcf607302f9eb1
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-06-18 20:21:13 +0530

    Cleanup function declarations in addsub.c

--- src/crypto/src/bignum/addsub.c
+++ src/crypto/src/bignum/addsub.c
@@ -10,10 +10,9 @@
 #include <bignum.h>
 #include <round.h>
 
-uint8_t bignum_add_words(uint64_t *r, uint64_t *a, uint64_t *b, uint32_t count);
-uint8_t bignum_sub_words(uint64_t *r, uint64_t *a, uint64_t *b, uint32_t count);
+uint8_t bignum_add_words(bn_word_t *r, bn_word_t *a, bn_word_t *b, uint32_t count);
+uint8_t bignum_sub_words(bn_word_t *r, bn_word_t *a, bn_word_t *b, uint32_t count);
 
-void bignum_increment(bn_word_t *r, uint32_t count);
 void bignum_2complement(bn_word_t *r, uint32_t count);
 
 static void bignum_uadd(bignum_t *r, bignum_t *a, bignum_t *b, uint32_t min_words, uint32_t total_words)
