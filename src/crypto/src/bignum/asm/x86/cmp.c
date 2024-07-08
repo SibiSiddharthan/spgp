3916c4f2b2395070484da00da6a8d350d17c49d2
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-07-08 23:23:26 +0530

    Add bignum_cmp_words for x86 msvc

---
+++ src/crypto/src/bignum/asm/x86/cmp.c
@@ -0,0 +1,46 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <stdint.h>
+#include <intrin.h>
+
+// If a > b ->  1
+// If a < b -> -1
+// If a = b ->  0
+int32_t bignum_cmp_words(uint64_t *a, uint64_t *b, uint32_t count)
+{
+	uint64_t result = 0;
+	uint8_t borrow = 0;
+
+	// Start from MSB
+	a += (count - 1);
+	b += (count - 1);
+
+	for (int32_t pos = 0; pos < count; ++pos, --a, --b)
+	{
+		borrow = _subborrow_u64(borrow, *a, *b, &result);
+
+		if (result == 0)
+		{
+			continue;
+		}
+
+		// a < b
+		if (borrow != 0)
+		{
+			return -1;
+		}
+		// a > b
+		else
+		{
+			return 1;
+		}
+	}
+
+	// a = b
+	return 0;
+}
