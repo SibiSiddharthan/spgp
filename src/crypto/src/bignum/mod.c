4a8f7b31b7f854a941916ac84f1adbc1aa70fc88
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-07-09 22:30:04 +0530

    Implement bignum mod operations for addition and subraction

---
+++ src/crypto/src/bignum/mod.c
@@ -0,0 +1,74 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <bignum.h>
+#include <round.h>
+#include <minmax.h>
+
+bignum_t *bignum_modadd(bignum_t *r, bignum_t *a, bignum_t *b, bignum_t *m)
+{
+	bignum_t *temp = NULL;
+	uint32_t required_bits = m->bits;
+	uint32_t op_bits = MAX(a->bits, b->bits) + 1;
+
+	// Check zero.
+	if (m->bits == 0)
+	{
+		return NULL;
+	}
+
+	temp = bignum_add(NULL, a, b);
+	r = bignum_resize(r, required_bits);
+
+	if (temp == NULL || r == NULL)
+	{
+		return NULL;
+	}
+
+	r = bignum_mod(r, temp, m);
+	bignum_delete(temp);
+
+	if (r == NULL)
+	{
+		return NULL;
+	}
+
+	return r;
+}
+
+bignum_t *bignum_modsub(bignum_t *r, bignum_t *a, bignum_t *b, bignum_t *m)
+{
+	bignum_t *temp = NULL;
+	uint32_t required_bits = m->bits;
+	uint32_t op_bits = MAX(a->bits, b->bits) + 1;
+
+	// Check zero.
+	if (m->bits == 0)
+	{
+		return NULL;
+	}
+
+	temp = bignum_sub(NULL, a, b);
+	r = bignum_resize(r, required_bits);
+
+	if (temp == NULL || r == NULL)
+	{
+		return NULL;
+	}
+
+	r = bignum_mod(r, temp, m);
+	bignum_delete(temp);
+
+	if (r == NULL)
+	{
+		return NULL;
+	}
+
+	return r;
+}
