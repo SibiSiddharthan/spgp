8ada9d8bd97eb70ba07d830b85ac198dbc24979d
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-08-04 22:08:54 +0530

    Update algorithms.h

--- src/core/algorithms.h
+++ src/core/algorithms.h
@@ -1,9 +1,9 @@
 #ifndef SPGP_ALGORITHMS_H
 #define SPGP_ALGORITHMS_H
 
-#include "macros.h"
+// Refer RFC 9580 - OpenPGP, Section 9 Constants
 
-typedef enum _public_key_algorithms
+typedef enum _pgp_public_key_algorithms
 {
 	PGP_RSA_ENCRYPT_OR_SIGN = 1,
 	PGP_RSA_ENCRYPT_ONLY = 2,
@@ -12,15 +12,14 @@ typedef enum _public_key_algorithms
 	PGP_DSA = 17,
 	PGP_ECDH = 18,
 	PGP_ECDSA = 19,
-	PGP_ELGAMAL_ENCRYPT_OR_SIGN = 20,
 	PGP_EDDSA_LEGACY = 22,
 	PGP_X25519 = 25,
 	PGP_X448 = 26,
 	PGP_ED25519 = 27,
 	PGP_ED448 = 28
-} public_key_algorithms;
+} pgp_public_key_algorithms;
 
-typedef enum _symmetric_key_algorithms
+typedef enum _pgp_symmetric_key_algorithms
 {
 	PGP_PLAINTEXT = 0,
 	PGP_IDEA = 1,
@@ -34,17 +33,17 @@ typedef enum _symmetric_key_algorithms
 	PGP_CAMELLIA_128 = 11,
 	PGP_CAMELLIA_192 = 12,
 	PGP_CAMELLIA_256 = 13
-} symmetric_key_algorithms;
+} pgp_symmetric_key_algorithms;
 
-typedef enum _compression_algorithms
+typedef enum _pgp_compression_algorithms
 {
 	UNCOMPRESSED = 0,
 	DEFALTE = 1,
 	ZLIB = 2,
 	BZIP2 = 3
-} compression_algorithms;
+} pgp_compression_algorithms;
 
-typedef enum _hash_algorithms
+typedef enum _pgp_hash_algorithms
 {
 	PGP_MD5 = 1,
 	PGP_SHA1 = 2,
@@ -55,13 +54,13 @@ typedef enum _hash_algorithms
 	PGP_SHA2_224 = 11,
 	PGP_SHA3_256 = 12,
 	PGP_SHA3_512 = 14
-} hash_algorithms;
+} pgp_hash_algorithms;
 
-typedef enum _aead_algorithms
+typedef enum _pgp_aead_algorithms
 {
 	PGP_AEAD_EAX = 1,
 	PGP_AEAD_OCB = 2,
 	PGP_AEAD_GCM = 3
-} aead_algorithms;
+} pgp_aead_algorithms;
 
 #endif
