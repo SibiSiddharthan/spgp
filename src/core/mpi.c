96b662bd52c68353272965abe30bcc01adf65098
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-08-04 22:43:56 +0530

    Implement mpi

---
+++ src/core/mpi.c
@@ -0,0 +1,102 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <spgp.h>
+#include <mpi.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+mpi_t *mpi_init_checked(void *ptr, size_t mpi_size, uint32_t bits)
+{
+	mpi_t *mpi = ptr;
+
+	memset(mpi, 0, mpi_size);
+	mpi->bits = bits;
+
+	return mpi;
+}
+
+mpi_t *mpi_init(void *ptr, size_t size, uint16_t bits)
+{
+	mpi_t *mpi = ptr;
+	size_t required_size = sizeof(mpi_t);
+
+	required_size += CEIL_DIV(mpi->bits, 8);
+
+	if (size < required_size)
+	{
+		return NULL;
+	}
+
+	return mpi_init_checked(mpi, required_size, bits);
+}
+
+mpi_t *mpi_new(uint16_t bits)
+{
+	mpi_t *mpi = NULL;
+	size_t size = sizeof(mpi_t);
+
+	size += CEIL_DIV(mpi->bits, 8);
+
+	mpi = (mpi_t *)malloc(size);
+
+	if (mpi == NULL)
+	{
+		return NULL;
+	}
+
+	return mpi_init_checked(mpi, size, bits);
+}
+
+void mpi_delete(mpi_t *mpi)
+{
+	free(mpi);
+}
+
+mpi_t *mpi_from_bn(mpi_t *mpi, bignum_t *bn)
+{
+	if (mpi == NULL)
+	{
+		mpi = mpi_new(bn->bits);
+
+		if (mpi == NULL)
+		{
+			return NULL;
+		}
+	}
+
+	if (mpi->bits < bn->bits)
+	{
+		return NULL;
+	}
+
+	// Get the bytes
+	mpi->bits = bn->bits;
+	bignum_get_bytes_be(bn, mpi->bytes, CEIL_DIV(mpi->bits, 8));
+
+	return mpi;
+}
+
+// See RFC 9580 - OpenPGP, Section 3.2 Multiprecision Integers.
+int32_t mpi_write(mpi_t *mpi, void *ptr, size_t size)
+{
+	// 2 bytes for the bits + the number in big endian form.
+	uint16_t required_size = 2 + CEIL_DIV(mpi->bits, 8);
+	uint16_t bits_be = BSWAP_16(mpi->bits);
+	byte_t *out = ptr;
+
+	if (size < required_size)
+	{
+		return -1;
+	}
+
+	memcpy(out, &bits_be, sizeof(uint16_t));
+	memcpy(out + sizeof(uint16_t), mpi->bytes, CEIL_DIV(mpi->bits, 8));
+
+	return required_size;
+}
---
+++ src/core/mpi.h
@@ -0,0 +1,32 @@
+/*
+   Copyright (c) 2024 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef SPGP_MPI_H
+#define SPGP_MPI_H
+
+#include <spgp.h>
+#include <bignum.h>
+
+typedef struct _mpi_t
+{
+	uint16_t bits;
+	byte_t *bytes;
+} mpi_t;
+
+inline size_t mpi_size(uint16_t bits)
+{
+	return sizeof(mpi_t) + CEIL_DIV(bits, 8);
+}
+
+mpi_t *mpi_init(void *ptr, size_t size, uint16_t bits);
+mpi_t *mpi_new(uint16_t bits);
+void mpi_delete(mpi_t *mpi);
+
+mpi_t *mpi_from_bn(mpi_t *mpi, bignum_t *bn);
+int32_t mpi_write(mpi_t *mpi, void *ptr, size_t size);
+
+#endif
