fbaefee02ae8b0135aa1651f19b74ce6e9f3fb30
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-08-05 23:33:18 +0530

    Updata spgp.h

--- src/core/spgp.h
+++ src/core/spgp.h
@@ -5,9 +5,14 @@
    Refer to the LICENSE file at the root directory for details.
 */
 
+#ifndef SPGP_H
+#define SPGP_H
+
 #include <types.h>
 #include <buffer.h>
 
 #include <byteswap.h>
 #include <minmax.h>
 #include <round.h>
+
+#endif
