f751a9174357cfee216b7384eb7d3905f6c0c61a
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-11-21 22:20:41 +0530

    Update spgp.c

--- src/core/spgp.c
+++ src/core/spgp.c
@@ -5,21 +5,29 @@
    Refer to the LICENSE file at the root directory for details.
 */
 
-#include <stdio.h>
+#include <spgp.h>
+#include <packet.h>
 
-#include "packet.h"
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	char buffer[65536] = {0};
+	char buffer[65536];
 	size_t size = 0;
 
-	const char *filename = "test.c.sig";
+	for (int i = 1; i < argc; ++i)
+	{
+		FILE *file = fopen(argv[i], "rb");
 
-	FILE *f = fopen(filename, "rb");
-	size = fread(buffer, 1, 65536, f);
+		if (file == NULL)
+		{
+			printf("%s not found.\n", argv[i]);
+			break;
+		}
 
-	dump_pgp_packet(buffer, size);
+		size = fread(buffer, 1, 65536, file);
+		fclose(file);
+	}
 
 	return 0;
 }
