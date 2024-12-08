1169b0f49d8a199590ab9f513b58022bcb9af1d1
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-12-08 19:03:06 +0530

    Implement ed25519ph signature generation and verification

--- src/crypto/src/signature/eddsa.c
+++ src/crypto/src/signature/eddsa.c
@@ -46,6 +46,7 @@ static ed25519_signature *ed25519_sign_internal(ec_group *group, ed25519_key *ke
 	y = bignum_ctx_allocate_bignum(bctx, group->bits);
 	k = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
 	s = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
+	d = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
 
 	// Compute the prehash
 	sha512_update(&hctx, key->private_key, ED25519_KEY_OCTETS);
@@ -59,7 +60,8 @@ static ed25519_signature *ed25519_sign_internal(ec_group *group, ed25519_key *ke
 	sha512_reset(&hctx);
 
 	k = bignum_set_bytes_le(k, hash, SHA512_HASH_SIZE);
-	s = bignum_set_bytes_le(k, prehash, SHA512_HASH_SIZE);
+	s = bignum_set_bytes_le(s, prehash, SHA512_HASH_SIZE);
+	d = bignum_set_bytes_le(d, prehash, SHA512_HASH_SIZE);
 
 	// R = [k]G.
 	r.x = x;
@@ -72,6 +74,8 @@ static ed25519_signature *ed25519_sign_internal(ec_group *group, ed25519_key *ke
 	sha512_update(&hctx, key->public_key, ED25519_KEY_OCTETS);
 	sha512_update(&hctx, message, size);
 	sha512_final(&hctx, hash);
+
+	d = bignum_set_bytes_le(d, hash, SHA512_HASH_SIZE);
 
 	// s = (r + ds) mod n.
 	s = bignum_modmul(bctx, s, s, d, group->n);
@@ -431,3 +435,224 @@ uint32_t ed448_verify(ed448_key *key, ed448_signature *edsign, void *context, si
 
 	return status;
 }
+
+static ed25519_signature *ed25519ph_sign_internal(ec_group *group, ed25519_key *key, ed25519_signature *edsign, void *context,
+												  size_t context_size, void *message, size_t message_size)
+{
+	bignum_ctx *bctx = group->bctx;
+	size_t ctx_size = 2 * bignum_size(group->bits) + 3 * bignum_size(SHA512_HASH_SIZE * 8);
+
+	bignum_t *k = NULL;
+	bignum_t *s = NULL;
+	bignum_t *d = NULL;
+
+	bignum_t *x = NULL;
+	bignum_t *y = NULL;
+
+	ec_point r;
+
+	sha512_ctx hctx;
+
+	byte_t prehash[SHA512_HASH_SIZE];
+	byte_t mhash[SHA512_HASH_SIZE];
+	byte_t hash[SHA512_HASH_SIZE];
+
+	bignum_ctx_start(bctx, ctx_size);
+
+	x = bignum_ctx_allocate_bignum(bctx, group->bits);
+	y = bignum_ctx_allocate_bignum(bctx, group->bits);
+	k = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
+	s = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
+	d = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
+
+	sha512_init(&hctx, sizeof(sha512_ctx));
+
+	// Hash the message
+	sha512_update(&hctx, message, message_size);
+	sha512_final(&hctx, mhash);
+	sha512_reset(&hctx);
+
+	// Compute the prehash
+	sha512_update(&hctx, key->private_key, ED25519_KEY_OCTETS);
+	sha512_final(&hctx, prehash);
+	sha512_reset(&hctx);
+
+	// Compute the final hash
+	sha512_update(&hctx, "SigEd25519 no Ed25519 collisions", 32);
+	sha512_update(&hctx, "\x01", 1);
+	sha512_update(&hctx, &context_size, 1);
+	sha512_update(&hctx, context, context_size);
+	sha512_update(&hctx, PTR_OFFSET(prehash, SHA512_HASH_SIZE / 2), SHA512_HASH_SIZE / 2);
+	sha512_update(&hctx, mhash, SHA512_HASH_SIZE);
+	sha512_final(&hctx, hash);
+	sha512_reset(&hctx);
+
+	k = bignum_set_bytes_le(k, hash, SHA512_HASH_SIZE);
+	s = bignum_set_bytes_le(s, prehash, SHA512_HASH_SIZE);
+
+	// R = [k]G.
+	r.x = x;
+	r.y = y;
+
+	ec_point_multiply(group, &r, group->g, k);
+	ec_point_encode(group, &r, edsign, ED25519_KEY_OCTETS, 0);
+
+	sha512_update(&hctx, "SigEd25519 no Ed25519 collisions", 32);
+	sha512_update(&hctx, "\x01", 1);
+	sha512_update(&hctx, &context_size, 1);
+	sha512_update(&hctx, context, context_size);
+	sha512_update(&hctx, edsign, ED25519_KEY_OCTETS);
+	sha512_update(&hctx, key->public_key, ED25519_KEY_OCTETS);
+	sha512_update(&hctx, mhash, SHA512_HASH_SIZE);
+	sha512_final(&hctx, hash);
+
+	d = bignum_set_bytes_le(d, hash, SHA512_HASH_SIZE);
+
+	// s = (r + ds) mod n.
+	s = bignum_modmul(bctx, s, s, d, group->n);
+	s = bignum_modadd(bctx, s, s, k, group->n);
+
+	bignum_get_bytes_le(s, PTR_OFFSET(edsign, ED25519_KEY_OCTETS), ED25519_KEY_OCTETS);
+
+	bignum_ctx_end(bctx);
+
+	return edsign;
+}
+
+ed25519_signature *ed25519ph_sign(ed25519_key *key, void *context, size_t context_size, void *message, size_t message_size, void *signature,
+								size_t signature_size)
+{
+	ec_group *group = NULL;
+	ed25519_signature *edsign = signature;
+
+	// Allocate the signature
+	if (edsign == NULL)
+	{
+		edsign = malloc(ED25519_SIGN_OCTETS);
+	}
+	else
+	{
+		if (signature_size < ED25519_SIGN_OCTETS)
+		{
+			return NULL;
+		}
+	}
+
+	if (edsign == NULL)
+	{
+		return NULL;
+	}
+
+	// Allocate the group
+	group = ec_group_new(EC_ED25519);
+
+	if (group == NULL)
+	{
+		if (signature == NULL)
+		{
+			free(edsign);
+		}
+
+		return NULL;
+	}
+
+	edsign = ed25519ph_sign_internal(group, key, edsign, context, context_size, message, message_size);
+
+	ec_group_delete(group);
+
+	return edsign;
+}
+
+static uint32_t ed25519ph_verify_internal(ec_group *group, ed25519_key *key, ed25519_signature *edsign, void *context, size_t context_size,
+										  void *message, size_t message_size)
+{
+	uint32_t status = 0;
+
+	bignum_ctx *bctx = group->bctx;
+	size_t ctx_size = 10 * bignum_size(group->bits);
+
+	bignum_t *t = NULL;
+	bignum_t *u = NULL;
+
+	ec_point *r = NULL, *q = NULL;
+	ec_point *lhs = NULL, *rhs = NULL;
+
+	sha512_ctx hctx;
+
+	byte_t hash[SHA512_HASH_SIZE];
+
+	bignum_ctx_start(bctx, ctx_size);
+
+	t = bignum_ctx_allocate_bignum(bctx, group->bits);
+	u = bignum_ctx_allocate_bignum(bctx, SHA512_HASH_SIZE * 8);
+
+	t = bignum_set_bytes_le(t, PTR_OFFSET(edsign, ED25519_KEY_OCTETS), ED25519_KEY_OCTETS);
+
+	if (bignum_cmp(t, group->n) >= 0)
+	{
+		goto end;
+	}
+
+	r = ec_point_decode(group, r, edsign, ED25519_KEY_OCTETS);
+
+	if (r == NULL)
+	{
+		goto end;
+	}
+
+	q = ec_point_decode(group, q, key->public_key, ED25519_KEY_OCTETS);
+
+	if (q == NULL)
+	{
+		goto end;
+	}
+
+	sha512_init(&hctx, sizeof(sha512_ctx));
+
+	sha512_update(&hctx, "SigEd25519 no Ed25519 collisions", 32);
+	sha512_update(&hctx, "\x01", 1);
+	sha512_update(&hctx, &context_size, 1);
+	sha512_update(&hctx, context, context_size);
+	sha512_update(&hctx, edsign, 32);
+	sha512_update(&hctx, key->public_key, ED25519_KEY_OCTETS);
+	sha512_update(&hctx, message, message_size);
+	sha512_final(&hctx, hash);
+
+	u = bignum_set_bytes_le(u, hash, SHA512_HASH_SIZE);
+
+	lhs = ec_point_multiply(group, lhs, group->g, t);
+
+	rhs = ec_point_multiply(group, rhs, q, u);
+	rhs = ec_point_add(group, rhs, rhs, r);
+
+	if (bignum_cmp(rhs->y, lhs->y) == 0)
+	{
+		status = 1;
+	}
+
+end:
+	bignum_ctx_end(bctx);
+
+	return status;
+}
+
+uint32_t ed25519ph_verify(ed25519_key *key, ed25519_signature *edsign, void *context, size_t context_size, void *message, size_t message_size)
+{
+	uint32_t status = 0;
+	ec_group *group = NULL;
+
+	// Allocate the group
+	group = ec_group_new(EC_ED25519);
+
+	if (group == NULL)
+	{
+		return 0;
+	}
+
+	status = ed25519_verify_internal(group, key, edsign, context, context_size, message, message_size);
+
+	ec_group_delete(group);
+
+	return status;
+}
+
