a76b18e8c6655fbba04f1e5f45796e566d02a9fe
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-12-16 18:57:16 +0530

    Refactor DSA parameter generation

--- src/crypto/src/signature/dsa.c
+++ src/crypto/src/signature/dsa.c
@@ -50,6 +50,152 @@ static uint32_t dsa_valid_pq_lengths(uint32_t p_bits, uint32_t q_bits)
 	return 1;
 }
 
+static bignum_t *dsa_generate_candidate_q(hash_ctx *hctx, bignum_t *qc, void *seed, size_t seed_size, uint32_t q_bits)
+{
+	drbg_ctx *drbg = get_default_drbg();
+
+	uint32_t hash_size = 0;
+	uint32_t hash_offset = 0;
+
+	uint32_t n = (q_bits / 8);
+
+	byte_t hash[MAX_HASH_SIZE] = {0};
+
+	if (drbg == NULL)
+	{
+		return NULL;
+	}
+
+	drbg_generate(drbg, 0, NULL, 0, seed, seed_size);
+
+	hash_reset(hctx);
+	hash_update(hctx, seed, seed_size);
+	hash_final(hctx, hash, MAX_HASH_SIZE);
+
+	// Truncate to q_bits
+	hash_size = MIN(hctx->hash_size, n);
+	hash_offset = hctx->hash_size - hash_size;
+
+	// Set the first and last bits to 1.
+	bignum_set_bytes_be(qc, PTR_OFFSET(hash, hash_offset), hash_size);
+	bignum_set_bit(qc, 0);
+	bignum_set_bit(qc, q_bits - 1);
+
+	return qc;
+}
+
+static bignum_t *dsa_generate_candidate_p(bignum_ctx *bctx, hash_ctx *hctx, bignum_t *pc, bignum_t *q, void *seed, size_t seed_size,
+										  uint32_t p_bits, uint32_t q_bits, uint32_t offset)
+{
+	bignum_t *q2 = NULL, *ds = NULL, *c = NULL;
+
+	uint32_t hash_size = 0;
+	uint32_t hash_offset = 0;
+
+	uint32_t l = (p_bits / 8);
+	uint32_t s = CEIL_DIV(l, hctx->hash_size);
+
+	uint32_t start = l;
+	uint32_t remaining = l;
+
+	void *pseed = NULL;
+	void *pbuffer = NULL;
+
+	byte_t hash[MAX_HASH_SIZE] = {0};
+
+	bignum_ctx_start(bctx, (2 * bignum_size(q_bits + 1)) + bignum_size((seed_size + 1) * 8) + seed_size + l);
+
+	q2 = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
+	ds = bignum_ctx_allocate_bignum(bctx, (seed_size + 1) * 8);
+	c = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
+
+	pseed = bignum_ctx_allocate_raw(bctx, seed_size);
+	pbuffer = bignum_ctx_allocate_raw(bctx, l);
+
+	ds = bignum_set_bytes_be(ds, seed, seed_size);
+	ds = bignum_uadd_word(ds, ds, offset);
+
+	for (uint32_t j = 0; j < s; ++j)
+	{
+		ds = bignum_uadd_word(ds, ds, j);
+		ds = bignum_umod2p(ds, seed_size * 8);
+
+		bignum_get_bytes_be(ds, pseed, seed_size);
+
+		hash_reset(hctx);
+		hash_update(hctx, seed, seed_size);
+		hash_final(hctx, hash, MAX_HASH_SIZE);
+
+		hash_size = MIN(hctx->hash_size, remaining);
+		hash_offset = hctx->hash_size - hash_size;
+		start -= hash_size;
+
+		memcpy(PTR_OFFSET(pbuffer, start), PTR_OFFSET(hash, hash_offset), hash_size);
+	}
+
+	bignum_set_bytes_be(pc, pbuffer, l);
+	bignum_set_bit(pc, p_bits - 1);
+
+	q2 = bignum_lshift1(q2, q);
+	c = bignum_mod(bctx, c, pc, q2);
+	pc = bignum_sub(pc, pc, c);
+	pc = bignum_uadd_word(pc, pc, 1);
+
+	bignum_ctx_end(bctx);
+
+	return pc;
+}
+
+static bignum_t *dsa_generate_candidate_g(bignum_ctx *bctx, hash_ctx *hctx, bignum_t *gc, bignum_t *p, bignum_t *q, void *seed,
+										  size_t seed_size, uint32_t p_bits)
+{
+	bignum_t *e = NULL, *w = NULL;
+
+	uint32_t l = (p_bits / 8);
+
+	uint16_t count_be = 0;
+	uint8_t index = 1;
+
+	byte_t hash[MAX_HASH_SIZE] = {0};
+
+	bignum_ctx_start(bctx, bignum_size(p_bits) + bignum_size(hctx->hash_size * 8));
+
+	e = bignum_ctx_allocate_bignum(bctx, p_bits);
+	w = bignum_ctx_allocate_bignum(bctx, hctx->hash_size * 8);
+
+	e = bignum_usub_word(e, p, 1);
+	e = bignum_div(bctx, e, e, q);
+
+	for (uint16_t c = 1; c < (4 * l); ++c)
+	{
+		count_be = BSWAP_16(c);
+
+		hash_reset(hctx);
+		hash_update(hctx, seed, seed_size);
+		hash_update(hctx, "ggen", 4);
+		hash_update(hctx, &index, 1);
+		hash_update(hctx, &count_be, 2);
+		hash_final(hctx, hash, MAX_HASH_SIZE);
+
+		w = bignum_set_bytes_be(w, hash, hctx->hash_size);
+		gc = bignum_modexp(bctx, gc, w, e, p);
+
+		if (gc->bits >= 2)
+		{
+			break;
+		}
+
+		if (c == ((4 * l) - 1))
+		{
+			gc = NULL;
+		}
+	}
+
+	bignum_ctx_end(bctx);
+
+	return gc;
+}
+
 uint32_t dsa_parameters_generate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignum_t *g, uint32_t p_bits, uint32_t q_bits, void *seed,
 								 size_t seed_size)
 {
@@ -57,23 +203,14 @@ uint32_t dsa_parameters_generate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 	drbg_ctx *drbg = NULL;
 	bignum_ctx *bctx = NULL;
 
-	bignum_t *q2 = NULL, *qc = NULL, *pc = NULL, *gc = NULL, *ds = NULL, *c = NULL, *e = NULL, *w = NULL;
+	bignum_t *qc = NULL, *pc = NULL, *gc = NULL;
 	size_t ctx_size = 0;
 
 	uint32_t counter = (uint32_t)-1;
 	uint32_t offset = 1;
 
 	uint32_t l = (p_bits / 8);
-	uint32_t n = (q_bits / 8);
 	uint32_t s = CEIL_DIV(l, hctx->hash_size);
-
-	byte_t hash[MAX_HASH_SIZE] = {0};
-
-	uint32_t hash_size = 0;
-	uint32_t hash_offset = 0;
-
-	void *pseed = NULL;
-	void *pbuffer = NULL;
 
 	// Check bits
 	if (dsa_valid_pq_lengths(p_bits, q_bits) == 0)
@@ -98,8 +235,7 @@ uint32_t dsa_parameters_generate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 		return (uint32_t)-1;
 	}
 
-	ctx_size = bignum_size(q_bits) + (3 * bignum_size(p_bits)) + bignum_size((seed_size + 1) * 8) + (2 * bignum_size(q_bits + 1)) +
-			   bignum_size(hctx->hash_size * 8) + seed_size + l;
+	ctx_size = bignum_size(q_bits) + (2 * bignum_size(p_bits));
 	bctx = bignum_ctx_new(256 + ctx_size);
 
 	if (bctx == NULL)
@@ -112,75 +248,30 @@ uint32_t dsa_parameters_generate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 	qc = bignum_ctx_allocate_bignum(bctx, q_bits);
 	pc = bignum_ctx_allocate_bignum(bctx, p_bits);
 	gc = bignum_ctx_allocate_bignum(bctx, p_bits);
-	ds = bignum_ctx_allocate_bignum(bctx, (seed_size + 1) * 8);
-
-	c = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
-	q2 = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
-
-	e = bignum_ctx_allocate_bignum(bctx, p_bits);
-	w = bignum_ctx_allocate_bignum(bctx, hctx->hash_size * 8);
-
-	pseed = bignum_ctx_allocate_raw(bctx, seed_size);
-	pbuffer = bignum_ctx_allocate_raw(bctx, l);
 
 	// Generate q
-retry_gen_q:
-	drbg_generate(drbg, 0, NULL, 0, seed, seed_size);
-
-	hash_reset(hctx);
-	hash_update(hctx, seed, seed_size);
-	hash_final(hctx, hash, MAX_HASH_SIZE);
-
-	// Truncate to q_bits
-	hash_size = MIN(hctx->hash_size, n);
-	hash_offset = hctx->hash_size - hash_size;
-
-	// Set the first and last bits to 1.
-	bignum_set_bytes_be(qc, PTR_OFFSET(hash, hash_offset), hash_size);
-	bignum_set_bit(qc, 0);
-	bignum_set_bit(qc, q_bits - 1);
-
-	// Check prime
-	if (bignum_is_probable_prime(bctx, qc) == 0)
+	do
 	{
-		goto retry_gen_q;
-	}
+		qc = dsa_generate_candidate_q(hctx, qc, seed, seed_size, q_bits);
+
+		if (qc == NULL)
+		{
+			return (uint32_t)-1;
+		}
+
+	} while (bignum_is_probable_prime(bctx, qc) == 0);
 
 	bignum_copy(q, qc);
 
 	// Generate p
 	for (uint32_t i = 0; i < (4 * l); ++i)
 	{
-		uint32_t start = l;
-		uint32_t remaining = l;
+		pc = dsa_generate_candidate_p(bctx, hctx, pc, q, seed, seed_size, p_bits, q_bits, offset);
 
-		ds = bignum_set_bytes_be(ds, seed, seed_size);
-		ds = bignum_uadd_word(ds, ds, offset);
-
-		for (uint32_t j = 0; j < s; ++j)
+		if (pc == NULL)
 		{
-			ds = bignum_uadd_word(ds, ds, j);
-			ds = bignum_mod2p(ds, ds, seed_size * 8);
-
-			bignum_get_bytes_be(ds, pseed, seed_size);
-
-			hash_reset(hctx);
-			hash_update(hctx, seed, seed_size);
-			hash_final(hctx, hash, MAX_HASH_SIZE);
-
-			hash_size = MIN(hctx->hash_size, remaining);
-			hash_offset = hctx->hash_size - hash_size;
-			start -= hash_size;
-
-			memcpy(PTR_OFFSET(pbuffer, start), PTR_OFFSET(hash, hash_offset), hash_size);
+			return (uint32_t)-1;
 		}
-
-		bignum_set_bytes_be(pc, pbuffer, l);
-		bignum_set_bit(pc, p_bits - 1);
-
-		c = bignum_mod(bctx, c, pc, q2);
-		pc = bignum_sub(pc, pc, c);
-		pc = bignum_uadd_word(pc, pc, 1);
 
 		if (bignum_get_bit(pc, p_bits - 1) == 0)
 		{
@@ -200,30 +291,11 @@ retry_gen_q:
 	bignum_copy(p, pc);
 
 	// Generate g
-	uint16_t count_be = 0;
-	uint8_t index = 1;
+	gc = dsa_generate_candidate_g(bctx, hctx, gc, p, q, seed, seed_size, p_bits);
 
-	e = bignum_usub_word(e, p, 1);
-	e = bignum_div(bctx, e, e, q);
-
-	for (uint16_t c = 1; c < (4 * l); ++c)
+	if (gc == NULL)
 	{
-		count_be = BSWAP_16(c);
-
-		hash_reset(hctx);
-		hash_update(hctx, seed, seed_size);
-		hash_update(hctx, "ggen", 4);
-		hash_update(hctx, &index, 1);
-		hash_update(hctx, &count_be, 2);
-		hash_final(hctx, hash, MAX_HASH_SIZE);
-
-		w = bignum_set_bytes_be(w, hash, hctx->hash_size);
-		gc = bignum_modexp(bctx, gc, w, e, p);
-
-		if (gc->bits >= 2)
-		{
-			break;
-		}
+		return (uint32_t)-1;
 	}
 
 	bignum_copy(g, gc);
@@ -238,25 +310,16 @@ uint32_t dsa_parameters_validate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 {
 	bignum_ctx *bctx = NULL;
 
+	bignum_t *qc = NULL, *pc = NULL, *gc = NULL;
+	size_t ctx_size = 0;
+
 	uint32_t p_bits = p->bits;
 	uint32_t q_bits = q->bits;
-
-	bignum_t *q2 = NULL, *qc = NULL, *pc = NULL, *gc = NULL, *ds = NULL, *c = NULL, *e = NULL, *w = NULL;
-	size_t ctx_size = 0;
 
 	uint32_t offset = 1;
 
 	uint32_t l = (p_bits / 8);
-	uint32_t n = (q_bits / 8);
 	uint32_t s = CEIL_DIV(l, hctx->hash_size);
-
-	byte_t hash[MAX_HASH_SIZE] = {0};
-
-	uint32_t hash_size = 0;
-	uint32_t hash_offset = 0;
-
-	void *pseed = NULL;
-	void *pbuffer = NULL;
 
 	// Check bits
 	if (dsa_valid_pq_lengths(p_bits, q_bits) == 0)
@@ -279,8 +342,7 @@ uint32_t dsa_parameters_validate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 		return 0;
 	}
 
-	ctx_size = bignum_size(q_bits) + (3 * bignum_size(p_bits)) + bignum_size((seed_size + 1) * 8) + (2 * bignum_size(q_bits + 1)) +
-			   bignum_size(hctx->hash_size * 8) + seed_size + l;
+	ctx_size = bignum_size(q_bits) + (2 * bignum_size(p_bits));
 	bctx = bignum_ctx_new(256 + ctx_size);
 
 	if (bctx == NULL)
@@ -293,37 +355,21 @@ uint32_t dsa_parameters_validate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 	qc = bignum_ctx_allocate_bignum(bctx, q_bits);
 	pc = bignum_ctx_allocate_bignum(bctx, p_bits);
 	gc = bignum_ctx_allocate_bignum(bctx, p_bits);
-	ds = bignum_ctx_allocate_bignum(bctx, (seed_size + 1) * 8);
-
-	c = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
-	q2 = bignum_ctx_allocate_bignum(bctx, q_bits + 1);
-
-	e = bignum_ctx_allocate_bignum(bctx, p_bits);
-	w = bignum_ctx_allocate_bignum(bctx, hctx->hash_size * 8);
-
-	pseed = bignum_ctx_allocate_raw(bctx, seed_size);
-	pbuffer = bignum_ctx_allocate_raw(bctx, l);
 
 	// Check q
-	hash_reset(hctx);
-	hash_update(hctx, seed, seed_size);
-	hash_final(hctx, hash, MAX_HASH_SIZE);
+	qc = dsa_generate_candidate_q(hctx, qc, seed, seed_size, q_bits);
 
-	// Truncate to q_bits
-	hash_size = MIN(hctx->hash_size, n);
-	hash_offset = hctx->hash_size - hash_size;
+	if (qc == NULL)
+	{
+		return 0;
+	}
 
-	// Set the first and last bits to 1.
-	bignum_set_bytes_be(qc, PTR_OFFSET(hash, hash_offset), hash_size);
-	bignum_set_bit(qc, 0);
-	bignum_set_bit(qc, q_bits - 1);
-
-	// Check prime
 	if (bignum_cmp(q, qc) != 0)
 	{
 		return 0;
 	}
 
+	// Check prime
 	if (bignum_is_probable_prime(bctx, qc) == 0)
 	{
 		return 0;
@@ -332,36 +378,12 @@ uint32_t dsa_parameters_validate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 	// Check p
 	for (uint32_t i = 0; i < (4 * l); ++i)
 	{
-		uint32_t start = l;
-		uint32_t remaining = l;
+		pc = dsa_generate_candidate_p(bctx, hctx, pc, q, seed, seed_size, p_bits, q_bits, offset);
 
-		ds = bignum_set_bytes_be(ds, seed, seed_size);
-		ds = bignum_uadd_word(ds, ds, offset);
-
-		for (uint32_t j = 0; j < s; ++j)
+		if (pc == NULL)
 		{
-			ds = bignum_uadd_word(ds, ds, j);
-			ds = bignum_mod2p(ds, ds, seed_size * 8);
-
-			bignum_get_bytes_be(ds, pseed, seed_size);
-
-			hash_reset(hctx);
-			hash_update(hctx, seed, seed_size);
-			hash_final(hctx, hash, MAX_HASH_SIZE);
-
-			hash_size = MIN(hctx->hash_size, remaining);
-			hash_offset = hctx->hash_size - hash_size;
-			start -= hash_size;
-
-			memcpy(PTR_OFFSET(pbuffer, start), PTR_OFFSET(hash, hash_offset), hash_size);
+			return 0;
 		}
-
-		bignum_set_bytes_be(pc, pbuffer, l);
-		bignum_set_bit(pc, p_bits - 1);
-
-		c = bignum_mod(bctx, c, pc, q2);
-		pc = bignum_sub(pc, pc, c);
-		pc = bignum_uadd_word(pc, pc, 1);
 
 		if (bignum_get_bit(pc, p_bits - 1) == 0)
 		{
@@ -386,30 +408,11 @@ uint32_t dsa_parameters_validate(hash_ctx *hctx, bignum_t *p, bignum_t *q, bignu
 	}
 
 	// Check g
-	uint16_t count_be = 0;
-	uint8_t index = 1;
+	gc = dsa_generate_candidate_g(bctx, hctx, gc, p, q, seed, seed_size, p_bits);
 
-	e = bignum_usub_word(e, p, 1);
-	e = bignum_div(bctx, e, e, q);
-
-	for (uint16_t c = 1; c < (4 * l); ++c)
+	if (gc == NULL)
 	{
-		count_be = BSWAP_16(c);
-
-		hash_reset(hctx);
-		hash_update(hctx, seed, seed_size);
-		hash_update(hctx, "ggen", 4);
-		hash_update(hctx, &index, 1);
-		hash_update(hctx, &count_be, 2);
-		hash_final(hctx, hash, MAX_HASH_SIZE);
-
-		w = bignum_set_bytes_be(w, hash, hctx->hash_size);
-		gc = bignum_modexp(bctx, gc, w, e, p);
-
-		if (gc->bits >= 2)
-		{
-			break;
-		}
+		return (uint32_t)-1;
 	}
 
 	if (bignum_cmp(g, gc) != 0)
