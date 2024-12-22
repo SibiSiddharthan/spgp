e92659b1d0dfb6713bce9bc85966cc6d8518ebf6
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-12-22 18:54:28 +0530

    Implement edwards curve point validation

--- src/crypto/src/ec/edwards.c
+++ src/crypto/src/ec/edwards.c
@@ -24,3 +24,40 @@ uint32_t ec_edwards_point_is_identity(ec_point *a)
 	return 0;
 }
 
+uint32_t ec_edwards_point_on_curve(ec_group *eg, ec_point *a)
+{
+	uint32_t result = 0;
+	ec_edwards_curve *parameters = eg->parameters;
+
+	bignum_t *lhs = NULL, *rhs = NULL;
+	bignum_t *xsq = NULL, *ysq = NULL;
+
+	bignum_ctx_start(eg->bctx, 2 * bignum_size(eg->bits) + 2 * bignum_size(eg->bits * 2));
+
+	lhs = bignum_ctx_allocate_bignum(eg->bctx, eg->bits);
+	rhs = bignum_ctx_allocate_bignum(eg->bctx, eg->bits);
+	xsq = bignum_ctx_allocate_bignum(eg->bctx, eg->bits * 2);
+	ysq = bignum_ctx_allocate_bignum(eg->bctx, eg->bits * 2);
+
+	xsq = bignum_sqr(eg->bctx, xsq, a->x);
+	ysq = bignum_sqr(eg->bctx, ysq, a->y);
+
+	// Compute (ax^2 + y^2) % p
+	lhs = bignum_modmul(eg->bctx, lhs, xsq, parameters->a, eg->p);
+	lhs = bignum_modadd(eg->bctx, lhs, lhs, ysq, eg->p);
+
+	// Compute (1 + d*x^2*y^2) % p
+	rhs = bignum_modmul(eg->bctx, rhs, xsq, ysq, eg->p);
+	rhs = bignum_modmul(eg->bctx, rhs, rhs, parameters->d, eg->p);
+	rhs = bignum_uadd_word(rhs, rhs, 1);
+
+	// Compare
+	if (bignum_cmp(lhs, rhs) == 0)
+	{
+		result = 1;
+	}
+
+	bignum_ctx_end(eg->bctx);
+
+	return result;
+}
