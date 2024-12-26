a1009f4bad555f74fdbedb790d11dc71816a4297
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2024-12-26 18:46:46 +0530

    Add kyber sampling functions

--- src/crypto/src/lattice/kyber.c
+++ src/crypto/src/lattice/kyber.c
@@ -52,3 +52,56 @@ static void byte_decode(void *out, void *in, uint8_t d)
 	}
 }
 
+static void sample_ntt(shake256_ctx *xof, uint16_t f[256], uint8_t b[34])
+{
+	byte_t c[3];
+	byte_t out[3 * 256] = {0};
+	uint16_t j = 0;
+	uint16_t d1 = 0, d2 = 0;
+
+	shake256_reset(xof, 2 * 256);
+	shake256_update(xof, b, 34);
+	shake256_final(xof, out, 3 * 256);
+
+	while (j < 256)
+	{
+		// Copyt 3 bytes
+		c[0] = out[j * 3];
+		c[1] = out[(j * 3) + 1];
+		c[2] = out[(j * 3) + 2];
+
+		d1 = c[0] + (256 * (c[1] % 16));
+		d2 = (c[1] / 16) + (16 * c[2]);
+
+		if (d1 < KYBER_Q)
+		{
+			f[j] = d1;
+			j += 1;
+		}
+		if (d2 < KYBER_Q && j < 256)
+		{
+			f[j] = d2;
+			j += 1;
+		}
+	}
+}
+
+static void sample_polycbd(uint16_t f[256], uint8_t *b, uint8_t e)
+{
+	for (uint16_t i = 0; i < KYBER_N; ++i)
+	{
+		uint16_t x = 0, y = 0;
+
+		for (uint16_t j = 0; j < e; ++j)
+		{
+			x += GET_BIT(b, (2 * i * e) + j);
+		}
+
+		for (uint16_t j = 0; j < e; ++j)
+		{
+			y += GET_BIT(b, (2 * i * e) + e + j);
+		}
+
+		f[i] = x >= y ? ((x - y) % KYBER_Q) : (KYBER_Q - ((y - x) % KYBER_Q));
+	}
+}
