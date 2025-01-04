15943f632ca356db08843967d371f8bb4768f391
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-01-05 04:08:20 +0530

    Add packet header printing

--- src/core/print.c
+++ src/core/print.c
@@ -12,6 +12,8 @@
 #include <stdio.h>
 #include <string.h>
 
+pgp_packet_type pgp_packet_get_type(byte_t tag);
+
 static const char hex_table[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
 static pgp_packet_header_type get_packet_header_type(void *packet)
@@ -26,6 +28,106 @@ static pgp_packet_header_type get_packet_header_type(void *packet)
 	}
 
 	return PGP_LEGACY_HEADER;
+}
+
+static size_t pgp_packet_header_print(pgp_packet_header header, void *str, size_t size)
+{
+	pgp_packet_header_type format = get_packet_header_type(&header);
+	pgp_packet_type type = pgp_packet_get_type(header.tag);
+
+	byte_t *out = str;
+	size_t pos = 0;
+
+	switch (type)
+	{
+	case PGP_PKESK:
+		memcpy(PTR_OFFSET(str, pos), "Public Key Encrypted Session Key Packet (Tag 1)", 47);
+		pos += 47;
+		break;
+	case PGP_SIG:
+		memcpy(PTR_OFFSET(str, pos), "Signature Packet (Tag 2)", 24);
+		pos += 25;
+		break;
+	case PGP_SKESK:
+		memcpy(PTR_OFFSET(str, pos), "Symmetric Key Encrypted Session Key Packet (Tag 3)", 50);
+		pos += 51;
+		break;
+	case PGP_OPS:
+		memcpy(PTR_OFFSET(str, pos), "One-Pass Signature Packet (Tag 4)", 33);
+		pos += 34;
+		break;
+	case PGP_SECKEY:
+		memcpy(PTR_OFFSET(str, pos), "Secret Key Packet (Tag 5)", 25);
+		pos += 26;
+		break;
+	case PGP_PUBKEY:
+		memcpy(PTR_OFFSET(str, pos), "Public Key Packet (Tag 6)", 25);
+		pos += 26;
+		break;
+	case PGP_SECSUBKEY:
+		memcpy(PTR_OFFSET(str, pos), "Secret Subkey Packet (Tag 7)", 28);
+		pos += 29;
+		break;
+	case PGP_COMP:
+		memcpy(PTR_OFFSET(str, pos), "Compressed Data Packet (Tag 8)", 30);
+		pos += 30;
+		break;
+	case PGP_SED:
+		memcpy(PTR_OFFSET(str, pos), "Symmetrically Encrypted Data Packet (Tag 9)", 43);
+		pos += 44;
+		break;
+	case PGP_MARKER:
+		memcpy(PTR_OFFSET(str, pos), "Marker Packet (Tag 10)", 22);
+		pos += 23;
+		break;
+	case PGP_LIT:
+		memcpy(PTR_OFFSET(str, pos), "Literal Data Packet (Tag 11)", 28);
+		pos += 28;
+		break;
+	case PGP_TRUST:
+		memcpy(PTR_OFFSET(str, pos), "Trust Packet (Tag 12)", 21);
+		pos += 22;
+		break;
+	case PGP_UID:
+		memcpy(PTR_OFFSET(str, pos), "User ID Packet (Tag 13)", 23);
+		pos += 24;
+		break;
+	case PGP_PUBSUBKEY:
+		memcpy(PTR_OFFSET(str, pos), "Public Subkey Packet (Tag 14)", 29);
+		pos += 30;
+		break;
+	case PGP_UAT:
+		memcpy(PTR_OFFSET(str, pos), "User Attribute Packet (Tag 17)", 30);
+		pos += 31;
+		break;
+	case PGP_SEIPD:
+		memcpy(PTR_OFFSET(str, pos), "Symmetrically Encrypted and Integrity Protected Data Packet (Tag 18)", 68);
+		pos += 68;
+		break;
+	case PGP_MDC:
+		memcpy(PTR_OFFSET(str, pos), "Modification Detection Code Packet (Tag 19)", 45);
+		pos += 45;
+		break;
+	case PGP_PADDING:
+		memcpy(PTR_OFFSET(str, pos), "Padding Packet (Tag 21)", 23);
+		pos += 24;
+		break;
+	default:
+		pos += snprintf(PTR_OFFSET(str, pos), size - pos, "Unknown Packet (Tag %hhu)", header.tag);
+	}
+
+	if (format == PGP_LEGACY_HEADER)
+	{
+		memcpy(PTR_OFFSET(str, pos), " (Old)\n", 7);
+		pos += 7;
+	}
+	else
+	{
+		out[pos] = '\n';
+		pos += 1;
+	}
+
+	return pos;
 }
 
 size_t pgp_compresed_packet_print(pgp_compresed_packet *packet, void *str, size_t size)
