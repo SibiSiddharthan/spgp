8b1cdb343fab50a807c8d0791e68d9e52ef05906
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-01-06 22:30:55 +0530

    Add shim for crypto ciphers in spgp

---
+++ src/core/ciphers.c
@@ -0,0 +1,71 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <spgp.h>
+#include <ciphers.h>
+
+static cipher_algorithm pgp_algorithm_to_cipher_algorithm(pgp_symmetric_key_algorithms algorithm)
+{
+	switch (algorithm)
+	{
+	case PGP_PLAINTEXT:
+	case PGP_IDEA:
+	case PGP_CAST5_128:
+	case PGP_BLOWFISH:
+		return 0; // Unimplemented
+	case PGP_TDES:
+		return CIPHER_TDES;
+	case PGP_AES_128:
+		return CIPHER_AES128;
+	case PGP_AES_192:
+		return CIPHER_AES192;
+	case PGP_AES_256:
+		return CIPHER_AES256;
+	case PGP_TWOFISH:
+		return CIPHER_TWOFISH256;
+	case PGP_CAMELLIA_128:
+		return CIPHER_CAMELLIA128;
+	case PGP_CAMELLIA_192:
+		return CIPHER_CAMELLIA192;
+	case PGP_CAMELLIA_256:
+		return CIPHER_CAMELLIA256;
+	default:
+		return 0;
+	}
+}
+
+byte_t pgp_symmetric_cipher_valid(pgp_symmetric_key_algorithms algorithm)
+{
+	switch (algorithm)
+	{
+	case PGP_PLAINTEXT:
+	case PGP_IDEA:
+	case PGP_TDES:
+	case PGP_CAST5_128:
+	case PGP_BLOWFISH:
+	case PGP_AES_128:
+	case PGP_AES_192:
+	case PGP_AES_256:
+	case PGP_TWOFISH:
+	case PGP_CAMELLIA_128:
+	case PGP_CAMELLIA_192:
+	case PGP_CAMELLIA_256:
+		return 1;
+	}
+
+	return 0;
+}
+
+byte_t pgp_symmetric_cipher_key_size(pgp_symmetric_key_algorithms algorithm)
+{
+	return (byte_t)cipher_key_size(pgp_algorithm_to_cipher_algorithm(algorithm));
+}
+
+byte_t pgp_symmetric_cipher_iv_size(pgp_symmetric_key_algorithms algorithm)
+{
+	return (byte_t)cipher_iv_size(pgp_algorithm_to_cipher_algorithm(algorithm));
+}
---
+++ src/core/ciphers.h
@@ -0,0 +1,29 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef SPGP_CIPHERS_H
+#define SPGP_CIPHERS_H
+
+#include <spgp.h>
+#include <algorithms.h>
+
+#include <cipher.h>
+
+byte_t pgp_symmetric_cipher_valid(pgp_symmetric_key_algorithms algorithm);
+
+byte_t pgp_symmetric_cipher_key_size(pgp_symmetric_key_algorithms algorithm);
+byte_t pgp_symmetric_cipher_iv_size(pgp_symmetric_key_algorithms algorithm);
+
+size_t pgp_aead_encrypt(pgp_symmetric_key_algorithms symmetric_key_algorithm_id, pgp_aead_algorithms aead_algorithm_id, void *key,
+						size_t key_size, void *iv, byte_t iv_size, void *associated_data, size_t ad_size, void *in, size_t in_size,
+						void *out, size_t out_size, void *tag, size_t tag_size);
+
+size_t pgp_aead_decrypt(pgp_symmetric_key_algorithms symmetric_key_algorithm_id, pgp_aead_algorithms aead_algorithm_id, void *key,
+						size_t key_size, void *iv, byte_t iv_size, void *associated_data, size_t ad_size, void *in, size_t in_size,
+						void *out, size_t out_size, void *tag, size_t tag_size);
+
+#endif
