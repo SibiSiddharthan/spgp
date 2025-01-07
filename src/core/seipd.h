85a9d54dddbe6ad68c5f8f7589e632fca3f70d06
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-01-08 01:19:54 +0530

    Add seipd packet handling functions

--- src/core/seipd.h
+++ src/core/seipd.h
@@ -36,11 +36,11 @@ typedef struct _pgp_seipd_packet
 	byte_t chunk_size;
 
 	byte_t salt[32];
+	byte_t tag[16];
 
 	uint16_t tag_size;
 	uint32_t data_size;
 
-	void *tag;
 	void *data;
 
 } pgp_seipd_packet;
@@ -48,7 +48,17 @@ typedef struct _pgp_seipd_packet
 pgp_sed_packet *pgp_sed_packet_read(pgp_sed_packet *packet, void *data, size_t size);
 size_t pgp_sed_packet_write(pgp_sed_packet *packet, void *ptr, size_t size);
 
+pgp_seipd_packet *pgp_seipd_packet_new(pgp_packet_header_type format, byte_t version, byte_t symmetric_key_algorithm_id,
+									   byte_t aead_algorithm_id, byte_t chunk_size);
+void pgp_seipd_packet_delete(pgp_seipd_packet *packet);
+
+pgp_seipd_packet *pgp_seipd_packet_encrypt(pgp_seipd_packet *packet, byte_t symmetric_key_algorithm_id, byte_t aead_algorithm_id,
+										   byte_t salt[32], void *session_key, size_t session_key_size, void *data, size_t data_size);
+size_t pgp_seipd_packet_decrypt(pgp_seipd_packet *packet, byte_t symmetric_key_algorithm_id, byte_t aead_algorithm_id, void *session_key,
+								size_t session_key_size, void *data, size_t data_size);
+
 pgp_seipd_packet *pgp_seipd_packet_read(pgp_seipd_packet *packet, void *data, size_t size);
 size_t pgp_seipd_packet_write(pgp_seipd_packet *packet, void *ptr, size_t size);
+size_t pgp_seipd_packet_print(pgp_seipd_packet *packet, void *str, size_t size);
 
 #endif
