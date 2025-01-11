81fbe632d48681e979edd62914ef51b5f56a74e3
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-01-11 14:31:31 +0530

    Rework pgp_packet_header_read

--- src/core/packet.c
+++ src/core/packet.c
@@ -18,81 +18,6 @@
 
 // Refer RFC 9580 - OpenPGP, Section 4.2 Packet Headers
 
-static uint32_t get_packet_body_size(void *packet, size_t packet_size)
-{
-	pgp_packet_header *header = packet;
-	byte_t *byte = (byte_t *)packet + 1;
-
-	// Packet size will be atleast 1.
-	byte_t tag = header->tag;
-	uint32_t size = 0;
-
-	if (packet_size < 2)
-	{
-		// No size field.
-		return 0;
-	}
-
-	// New format packet
-	if ((tag & 0xC0) == 0xC0)
-	{
-		// 5 octet length
-		if (byte[0] == 255)
-		{
-			if (packet_size < 6)
-			{
-				return 0;
-			}
-
-			size = (((uint32_t)byte[1] << 24) | ((uint32_t)byte[2] << 16) | ((uint32_t)byte[3] << 8) | (uint32_t)byte[4]);
-		}
-		// 2 octet legnth
-		else if (byte[0] >= 192 && byte[0] <= 233)
-		{
-			if (packet_size < 3)
-			{
-				return 0;
-			}
-
-			size = ((byte[0] - 192) << 8) + byte[1] + 192;
-		}
-		// 1 octed length
-		else if (byte[0] < 192)
-		{
-			size = byte[0];
-		}
-		// Partial body length
-		else
-		{
-			size = (uint32_t)1 << (byte[0] & 0x1F);
-		}
-	}
-	else
-	{
-		switch (tag & 0x3)
-		{
-		// 1 octed length
-		case 0:
-			size = byte[0];
-			break;
-		// 2 octet legnth
-		case 1:
-			size = (packet_size < 3) ? 0 : ((uint32_t)byte[0] << 8) + (uint32_t)byte[1];
-			break;
-		// 4 octet length
-		case 2:
-			size = (packet_size < 5) ? 0
-									 : ((uint32_t)byte[0] << 24) | ((uint32_t)byte[1] << 16) | ((uint32_t)byte[2] << 8) | (uint32_t)byte[3];
-			break;
-		// Legacy partial packets unsupported.
-		case 3:
-			return 0;
-		}
-	}
-
-	return size;
-}
-
 static byte_t get_packet_header_size(pgp_packet_header_format format, size_t size)
 {
 	if (format == PGP_HEADER)
@@ -252,16 +177,98 @@ pgp_packet_type pgp_packet_get_type(byte_t tag)
 pgp_packet_header pgp_packet_header_read(void *data, size_t size)
 {
 	byte_t *pdata = data;
-	pgp_packet_header header = {0};
 
-	if (size == 0)
+	pgp_packet_header error = {0};
+	pgp_packet_header header = {0};
+	pgp_packet_header_format format = PGP_HEADER;
+
+	if (size < 2)
 	{
 		return header;
 	}
 
+	// Get the tag
 	header.tag = pdata[0];
-	header.header_size = get_packet_header_size(PGP_PACKET_HEADER_FORMAT(header.tag), size);
-	header.body_size = get_packet_body_size(data, size);
+	format = PGP_PACKET_HEADER_FORMAT(header.tag);
+
+	// New format packet
+	if (format == PGP_HEADER)
+	{
+		// 5 octet length
+		if (pdata[1] == 255)
+		{
+			if (size < 6)
+			{
+				return error;
+			}
+
+			header.header_size = 6;
+			header.body_size = (((uint32_t)pdata[2] << 24) | ((uint32_t)pdata[3] << 16) | ((uint32_t)pdata[4] << 8) | (uint32_t)pdata[5]);
+		}
+		// 2 octet legnth
+		else if (pdata[1] >= 192 && pdata[2] <= 233)
+		{
+			if (size < 3)
+			{
+				return error;
+			}
+
+			header.header_size = 3;
+			header.body_size = ((pdata[1] - 192) << 8) + pdata[2] + 192;
+		}
+		// 1 octed length
+		else if (pdata[1] < 192)
+		{
+			header.header_size = 2;
+			header.body_size = pdata[1];
+		}
+		// Partial body length
+		else
+		{
+			header.header_size = 2;
+			header.body_size = (uint32_t)1 << (pdata[1] & 0x1F);
+		}
+	}
+	else
+	{
+		switch (header.tag & 0x3)
+		{
+		// 1 octed length
+		case 0:
+		{
+			header.header_size = 2;
+			header.body_size = pdata[1];
+		}
+		break;
+		// 2 octet legnth
+		case 1:
+		{
+			if (size < 3)
+			{
+				return error;
+			}
+
+			header.header_size = 3;
+			header.body_size = ((uint32_t)pdata[1] << 8) + (uint32_t)pdata[2];
+		}
+		break;
+		// 4 octet length
+		case 2:
+		{
+			if (size < 5)
+			{
+				return error;
+			}
+
+			header.header_size = 5;
+			header.body_size = ((uint32_t)pdata[1] << 24) | ((uint32_t)pdata[2] << 16) | ((uint32_t)pdata[3] << 8) | (uint32_t)pdata[4];
+		}
+		break;
+		// Legacy partial packets unsupported.
+		case 3:
+			return error;
+		}
+	}
 
 	return header;
 }
