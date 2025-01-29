1aa8ce70ac806f791978348008a5afcaef631466
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-01-30 02:12:53 +0530

    Add tests for ec_point_decode

--- src/crypto/tests/ec/test-ec-point.c
+++ src/crypto/tests/ec/test-ec-point.c
@@ -114,15 +114,15 @@ int32_t ec_point_encode_test_suite(void)
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 0);
-	CHECK_VALUE(result, 1);
-	CHECK_BLOCK(buffer, 1, "00");
+	status += CHECK_VALUE(result, 1);
+	status += CHECK_BLOCK(buffer, 1, "00");
 
 	result = 0;
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 1);
-	CHECK_VALUE(result, 1);
-	CHECK_BLOCK(buffer, 1, "00");
+	status += CHECK_VALUE(result, 1);
+	status += CHECK_BLOCK(buffer, 1, "00");
 
 	bignum_delete(x);
 	bignum_delete(y);
@@ -139,17 +139,18 @@ int32_t ec_point_encode_test_suite(void)
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 0);
-	CHECK_VALUE(result, 65);
-	CHECK_BLOCK(buffer, 65,
-				"04741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4f88f4b9463c7a024a98c7caab7784eab71146ed4ca45a358e66a00dd"
-				"32bb7e2c");
+	status += CHECK_VALUE(result, 65);
+	status += CHECK_BLOCK(
+		buffer, 65,
+		"04741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4f88f4b9463c7a024a98c7caab7784eab71146ed4ca45a358e66a00dd"
+		"32bb7e2c");
 
 	result = 0;
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 1);
-	CHECK_VALUE(result, 33);
-	CHECK_BLOCK(buffer, 33, "02741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4");
+	status += CHECK_VALUE(result, 33);
+	status += CHECK_BLOCK(buffer, 33, "02741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4");
 
 	bignum_delete(x);
 	bignum_delete(y);
@@ -166,27 +167,184 @@ int32_t ec_point_encode_test_suite(void)
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 0);
-	CHECK_VALUE(result, 65);
-	CHECK_BLOCK(buffer, 65,
-				"043ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d16f66df64333b375edb37bc505b0b3975f6f2fb26a16776251d071103"
-				"17d5c8bf");
+	status += CHECK_VALUE(result, 65);
+	status += CHECK_BLOCK(
+		buffer, 65,
+		"043ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d16f66df64333b375edb37bc505b0b3975f6f2fb26a16776251d071103"
+		"17d5c8bf");
 
 	result = 0;
 	memset(buffer, 0, 256);
 
 	result = ec_point_encode(group, &p, buffer, 256, 1);
-	CHECK_VALUE(result, 33);
-	CHECK_BLOCK(buffer, 33, "033ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d1");
+	status += CHECK_VALUE(result, 33);
+	status += CHECK_BLOCK(buffer, 33, "033ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d1");
 
 	bignum_delete(x);
 	bignum_delete(y);
 
 	// ---------------------------------------------------------------------------------------------------
 
+	ec_group_delete(group);
+
+	return status;
+}
+
+int32_t ec_point_decode_test_suite()
+{
+	int32_t status = 0;
+
+	ec_group *group = ec_group_new(EC_NIST_P256);
+	ec_point *p = NULL;
+
+	byte_t buffer[256] = {0};
+	char x[256] = {0}, y[256] = {0};
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(buffer, 1, "00");
+
+	p = ec_point_decode(group, NULL, buffer, 1);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x, "0x0", 3);
+	status += CHECK_HEX(y, "0x0", 3);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(
+		buffer, 65,
+		"04741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4f88f4b9463c7a024a98c7caab7784eab71146ed4ca45a358e66a00dd"
+		"32bb7e2c");
+
+	p = ec_point_decode(group, NULL, buffer, 65);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4", 64);
+	status += CHECK_HEX(y + 2, "f88f4b9463c7a024a98c7caab7784eab71146ed4ca45a358e66a00dd32bb7e2c", 64);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(buffer, 33, "02741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4");
+
+	p = ec_point_decode(group, NULL, buffer, 33);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "741dd5bda817d95e4626537320e5d55179983028b2f82c99d500c5ee8624e3c4", 64);
+	status += CHECK_HEX(y + 2, "f88f4b9463c7a024a98c7caab7784eab71146ed4ca45a358e66a00dd32bb7e2c", 64);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(
+		buffer, 65,
+		"043ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d16f66df64333b375edb37bc505b0b3975f6f2fb26a16776251d071103"
+		"17d5c8bf");
+
+	p = ec_point_decode(group, NULL, buffer, 65);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "3ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d1", 64);
+	status += CHECK_HEX(y + 2, "6f66df64333b375edb37bc505b0b3975f6f2fb26a16776251d07110317d5c8bf", 64);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(buffer, 33, "033ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d1");
+
+	p = ec_point_decode(group, NULL, buffer, 33);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "3ed113b7883b4c590638379db0c21cda16742ed0255048bf433391d374bc21d1", 64);
+	status += CHECK_HEX(y + 2, "6f66df64333b375edb37bc505b0b3975f6f2fb26a16776251d07110317d5c8bf", 64);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	ec_group_delete(group);
+	group = ec_group_new(EC_NIST_P224);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(buffer, 29, "02aea9e17a306517eb89152aa7096d2c381ec813c51aa880e7bee2c0fd");
+
+	p = ec_point_decode(group, NULL, buffer, 29);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "aea9e17a306517eb89152aa7096d2c381ec813c51aa880e7bee2c0fd", 56);
+	status += CHECK_HEX(y + 2, "c644cf154cc81f5ade49345e541b4d4b5c1adb3eb5c01c14ee949aa2", 56);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	memset(buffer, 0, 256);
+	memset(x, 0, 256);
+	memset(y, 0, 256);
+
+	hex_to_block(buffer, 29, "03858e6f9cc6c12c31f5df124aa77767b05c8bc021bd683d2b55571550");
+
+	p = ec_point_decode(group, NULL, buffer, 29);
+
+	bignum_get_hex(p->x, x, 256);
+	bignum_get_hex(p->y, y, 256);
+
+	status += CHECK_HEX(x + 2, "858e6f9cc6c12c31f5df124aa77767b05c8bc021bd683d2b55571550", 56);
+	status += CHECK_HEX(y + 2, "fb9232c15a3bc7673a3a03b0253824c53d0fd1411b1cabe2e187fb87", 56);
+
+	ec_point_delete(p);
+
+	// ---------------------------------------------------------------------------------------------------
+
+	ec_group_delete(group);
+
 	return status;
 }
 
 int main()
 {
-	return ec_nist_p384_test_suite() + ec_point_encode_test_suite();
+	return ec_nist_p384_test_suite() + ec_point_encode_test_suite() + ec_point_decode_test_suite();
 }
