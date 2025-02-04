ff7f2c7df8c2ae823593273f08293a22fbc8779e
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-02-04 23:23:44 +0530

    Add crypt.h

---
+++ src/crypto/include/crypt.h
@@ -0,0 +1,23 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef CRYPT_H
+#define CRYPT_H
+
+// Standard headers
+#include <stdint.h>
+#include <stddef.h>
+
+#include <compiler.h>
+#include <types.h>
+
+#include <minmax.h>
+#include <ptr.h>
+#include <round.h>
+#include <unused.h>
+
+#endif
