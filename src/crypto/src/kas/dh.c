0fa005236cf629f8c0efa76474327511709d2b14
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-02-16 14:52:03 +0530

    Update ECC CDH

--- src/crypto/src/kas/dh.c
+++ src/crypto/src/kas/dh.c
@@ -6,6 +6,7 @@
 */
 
 #include <kas.h>
+#include <bitscan.h>
 
 uint32_t ff_dh(dh_key *self_key, bignum_t *public_key, void *shared_secret, uint32_t size)
 {
@@ -48,6 +49,7 @@ uint32_t ec_dh(ec_key *self_key, ec_point *public_key, void *shared_secret, uint
 {
 	uint32_t result = 0;
 
+	uint32_t w = BSF_32(self_key->eg->cofactor);
 	ec_point *r = NULL;
 	bignum_t *d = NULL;
 
@@ -57,16 +59,17 @@ uint32_t ec_dh(ec_key *self_key, ec_point *public_key, void *shared_secret, uint
 	}
 
 	r = ec_point_new(self_key->eg);
-	d = bignum_new(self_key->eg->bits);
+	d = bignum_new(self_key->eg->bits + w);
 
 	if (r == NULL || d == NULL)
 	{
 		goto end;
 	}
 
-	// TODO: Optimize this
-	bignum_set_word(d, self_key->eg->cofactor);
-	d = bignum_modmul(self_key->eg->bctx, d, d, self_key->d, self_key->eg->n);
+	d = bignum_copy(d, self_key->d);
+	d = bignum_lshift(d, d, w);
+	d = bignum_mod(self_key->eg->bctx, d, d, self_key->eg->n);
+
 	r = ec_point_multiply(self_key->eg, r, public_key, d);
 
 	if (ec_point_is_identity(self_key->eg, r))
