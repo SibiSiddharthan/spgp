a0de0640e42d69cfab4e24cefb7bfd0deed12fcb
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-02-26 01:12:44 +0530

    Update implementation of pgp streams

--- src/core/stream.c
+++ src/core/stream.c
@@ -6,6 +6,7 @@
 */
 
 #include <spgp.h>
+#include <packet.h>
 #include <stream.h>
 
 #include <stdlib.h>
@@ -15,6 +16,9 @@ pgp_stream_t *pgp_stream_new(uint16_t capacity)
 {
 	pgp_stream_t *stream = NULL;
 	void *packets = NULL;
+
+	// Round to multiple of 4
+	capacity = ROUND_UP(capacity, 4);
 
 	stream = malloc(sizeof(pgp_stream_t));
 	packets = malloc(sizeof(void *) * capacity);
@@ -51,4 +55,97 @@ void pgp_stream_delete(pgp_stream_t *stream)
 
 	free(stream->packets);
 	free(stream);
+}
+
+pgp_stream_t *pgp_stream_read(void *data, size_t size)
+{
+	pgp_stream_t *stream = NULL;
+
+	size_t pos = 0;
+	uint16_t count = 0;
+
+	// Check packet header validity and count the packets.
+	while (pos < size)
+	{
+		pgp_packet_header header = pgp_packet_header_read(PTR_OFFSET(data, pos), size - pos);
+
+		if (header.tag == 0)
+		{
+			return NULL;
+		}
+
+		if ((header.body_size + header.header_size) > (size - pos))
+		{
+			return NULL;
+		}
+
+		pos += header.body_size + header.header_size;
+		count += 1;
+	}
+
+	stream = pgp_stream_new(count);
+
+	if (stream == NULL)
+	{
+		return NULL;
+	}
+
+	pos = 0;
+	count = 0;
+
+	// Read the packets
+	while (pos < size)
+	{
+		pgp_packet_header *header = NULL;
+		void *packet = pgp_packet_read(PTR_OFFSET(data, pos), size - pos);
+
+		if (packet == NULL)
+		{
+			pgp_stream_delete(stream);
+			return NULL;
+		}
+
+		stream->packets[count] = packet;
+
+		header = packet;
+		pos += header->body_size + header->header_size;
+		count += 1;
+	}
+
+	stream->count = count;
+
+	return stream;
+}
+
+size_t pgp_stream_write(pgp_stream_t *stream, void *buffer, size_t size, uint16_t options)
+{
+	pgp_packet_header *header = NULL;
+	size_t pos = 0;
+
+	for (uint16_t i = 0; i < stream->count; ++i)
+	{
+		header = stream->packets[i];
+
+		// Write complete packets only
+		if ((header->body_size + header->header_size) < (size - pos))
+		{
+			break;
+		}
+
+		pos += pgp_packet_write(stream->packets[i], PTR_OFFSET(buffer, pos), size - pos);
+	}
+
+	return pos;
+}
+
+size_t pgp_stream_print(pgp_stream_t *stream, void *buffer, size_t size, uint16_t options)
+{
+	size_t pos = 0;
+
+	for (uint16_t i = 0; i < stream->count; ++i)
+	{
+		pos += pgp_packet_print(stream->packets[i], PTR_OFFSET(buffer, pos), size - pos, options);
+	}
+
+	return pos;
 }
