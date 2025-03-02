5806440b0cbac8f99ccab8485b6d80857e950a27
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-03-03 01:18:05 +0530

    Initial io.c for windows

--- src/io/src/win32/io.c
+++ src/io/src/win32/io.c
@@ -1,10 +1,14 @@
-#[[
+/*
    Copyright (c) 2024 - 2025 Sibi Siddharthan
 
    Distributed under the MIT license.
    Refer to the LICENSE file at the root directory for details.
-]]
+*/
 
-add_subdirectory(core)
-add_subdirectory(crypto)
-add_subdirectory(io)
+#include <win32/nt.h>
+#include <win32/io.h>
+
+status_t os_close(handle_t handle)
+{
+	return NtClose(handle);
+}
