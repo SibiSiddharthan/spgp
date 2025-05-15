5b9e48b35ee9d8e908df97627219def358e4b0b9
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-05-15 23:53:28 +0530

    Implement aead and seipd encryption

--- src/core/app/encrypt.c
+++ src/core/app/encrypt.c
@@ -70,6 +70,104 @@ static pgp_stream_t *spgp_encrypt_mdc(pgp_key_packet **keys, uint32_t recipient_
 	return message;
 }
 
+static pgp_stream_t *spgp_encrypt_aead(pgp_key_packet **keys, uint32_t recipient_count, void **passphrases, uint32_t passphrase_count,
+									   pgp_stream_t *stream)
+{
+	pgp_stream_t *message = NULL;
+	pgp_aead_packet *aead = NULL;
+	pgp_pkesk_packet *pkesk = NULL;
+	pgp_skesk_packet *skesk = NULL;
+
+	byte_t session_key[32] = {0};
+	byte_t session_key_size = 0;
+
+	byte_t iv[16] = {0};
+	byte_t iv_size = pgp_aead_iv_size(PGP_AEAD_OCB);
+
+	pgp_s2k s2k = {0};
+
+	STREAM_CALL(message = pgp_stream_new(recipient_count + passphrase_count + 1));
+
+	// Process PKESKs first
+	for (uint32_t i = 0; i < recipient_count; ++i)
+	{
+		pkesk = NULL;
+
+		PGP_CALL(pgp_pkesk_packet_new(&pkesk, PGP_PKESK_V3));
+		PGP_CALL(pgp_pkesk_packet_session_key_encrypt(pkesk, keys[i], 0, PGP_AES_256, session_key, session_key_size));
+
+		pgp_stream_push(message, pkesk);
+	}
+
+	for (uint32_t i = 0; i < passphrase_count; ++i)
+	{
+		skesk = NULL;
+
+		PGP_CALL(pgp_skesk_packet_new(&skesk, PGP_SKESK_V5, PGP_AES_256, PGP_AEAD_OCB, &s2k));
+		PGP_CALL(pgp_skesk_packet_session_key_encrypt(skesk, passphrases[0], strlen(passphrases[0]), iv, iv_size, session_key,
+													  session_key_size));
+
+		pgp_stream_push(message, skesk);
+	}
+
+	PGP_CALL(pgp_aead_packet_new(&aead, PGP_AES_256, PGP_AEAD_OCB, PGP_MAX_CHUNK_SIZE));
+	PGP_CALL(pgp_aead_packet_encrypt(aead, iv, iv_size, session_key, session_key_size, stream));
+
+	pgp_stream_push(message, aead);
+
+	return message;
+}
+
+static pgp_stream_t *spgp_encrypt_seipd(pgp_key_packet **keys, uint32_t recipient_count, void **passphrases, uint32_t passphrase_count,
+										pgp_stream_t *stream)
+{
+	pgp_stream_t *message = NULL;
+	pgp_seipd_packet *seipd = NULL;
+	pgp_pkesk_packet *pkesk = NULL;
+	pgp_skesk_packet *skesk = NULL;
+
+	byte_t session_key[32] = {0};
+	byte_t session_key_size = 0;
+
+	byte_t iv[16] = {0};
+	byte_t iv_size = 0;
+
+	byte_t salt[32] = {0};
+
+	pgp_s2k s2k = {0};
+
+	STREAM_CALL(message = pgp_stream_new(recipient_count + passphrase_count + 1));
+
+	// Process PKESKs first
+	for (uint32_t i = 0; i < recipient_count; ++i)
+	{
+		pkesk = NULL;
+
+		PGP_CALL(pgp_pkesk_packet_new(&pkesk, PGP_PKESK_V6));
+		PGP_CALL(pgp_pkesk_packet_session_key_encrypt(pkesk, keys[i], 0, PGP_AES_256, session_key, session_key_size));
+
+		pgp_stream_push(message, pkesk);
+	}
+
+	for (uint32_t i = 0; i < passphrase_count; ++i)
+	{
+		skesk = NULL;
+
+		PGP_CALL(pgp_skesk_packet_new(&skesk, PGP_SKESK_V6, PGP_AES_256, PGP_AEAD_OCB, &s2k));
+		PGP_CALL(pgp_skesk_packet_session_key_encrypt(skesk, passphrases[0], strlen(passphrases[0]), iv, iv_size, session_key,
+													  session_key_size));
+
+		pgp_stream_push(message, skesk);
+	}
+
+	PGP_CALL(pgp_seipd_packet_new(&seipd, PGP_SEIPD_V2, PGP_AES_256, PGP_AEAD_OCB, PGP_MAX_CHUNK_SIZE));
+	PGP_CALL(pgp_seipd_packet_encrypt(seipd, salt, session_key, session_key_size, stream));
+
+	pgp_stream_push(message, seipd);
+
+	return message;
+}
+
 void spgp_encrypt(void)
 {
 	pgp_key_packet *key[16] = {0};
@@ -142,9 +240,11 @@ void spgp_encrypt(void)
 		{
 		case SPGP_MODE_RFC2440:
 		case SPGP_MODE_RFC4880:
-		case SPGP_MODE_LIBREPGP:
-		case SPGP_MODE_OPENPGP:
 			message = spgp_encrypt_mdc(key, count, command.passhprases->packets, command.passhprases->count, stream);
+		case SPGP_MODE_LIBREPGP:
+			message = spgp_encrypt_aead(key, count, command.passhprases->packets, command.passhprases->count, stream);
+		case SPGP_MODE_OPENPGP:
+			message = spgp_encrypt_seipd(key, count, command.passhprases->packets, command.passhprases->count, stream);
 		}
 
 		stream = pgp_stream_clear(stream, pgp_packet_delete);
@@ -160,9 +260,11 @@ void spgp_encrypt(void)
 			{
 			case SPGP_MODE_RFC2440:
 			case SPGP_MODE_RFC4880:
-			case SPGP_MODE_LIBREPGP:
-			case SPGP_MODE_OPENPGP:
 				message = spgp_encrypt_mdc(key, count, command.passhprases->packets, command.passhprases->count, stream);
+			case SPGP_MODE_LIBREPGP:
+				message = spgp_encrypt_aead(key, count, command.passhprases->packets, command.passhprases->count, stream);
+			case SPGP_MODE_OPENPGP:
+				message = spgp_encrypt_seipd(key, count, command.passhprases->packets, command.passhprases->count, stream);
 			}
 
 			stream = pgp_stream_clear(stream, pgp_packet_delete);
