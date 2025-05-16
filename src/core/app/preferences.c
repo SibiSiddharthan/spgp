2e51aec7d224157e821f3a9bafdaab420e083ef7
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-05-16 23:23:24 +0530

    Implement cipher aead algorithm preferences

--- src/core/app/preferences.c
+++ src/core/app/preferences.c
@@ -156,3 +156,61 @@ pgp_symmetric_key_algorithms preferred_cipher_algorithm(pgp_user_info **users, u
 
 	return algorithm;
 }
+
+uint16_t preferred_aead_algorithm(pgp_user_info **users, uint32_t count)
+{
+	pgp_symmetric_key_algorithms cipher_algorithm = 0;
+	pgp_aead_algorithms aead_algorithm = 0;
+	byte_t algorithm = 0;
+	byte_t cipher_algorithm_count = 8; // Actually only 7 are supported.
+
+	byte_t preferences[24] = {0};
+
+	// Encode as a 24 byte array.
+	// AES-128 EAX, AES-192 EAX ... CAMELLIA-256 EAX 0(pad) 8
+	// AES-128 OCB, AES-192 OCB ... CAMELLIA-256 OCB 0(pad) 8
+	// AES-128 GCM, AES-192 GCM ... CAMELLIA-256 GCM 0(pad) 8
+
+	for (uint32_t i = 0; i < count; ++i)
+	{
+		for (byte_t j = 0; j < users[i]->aead_algorithm_preferences_octets; j += 2)
+		{
+			preferences[((users[i]->aead_algorithm_preferences[j][1] - 1) * cipher_algorithm_count) +
+						(users[i]->aead_algorithm_preferences[j][0] - cipher_algorithm_count)] += 1;
+		}
+	}
+
+	for (uint32_t i = 0; i < 24; ++i)
+	{
+		if (preferences[i] == count)
+		{
+			if (algorithm == 0)
+			{
+				algorithm = preferences[i];
+			}
+			else
+			{
+				// Choose the stronger encryption algorithm
+				if (pgp_symmetric_cipher_key_size(preferences[i] % cipher_algorithm_count) >
+					pgp_symmetric_cipher_key_size(algorithm % cipher_algorithm_count))
+				{
+					algorithm = preferences[i];
+				}
+			}
+		}
+	}
+
+	if (algorithm == 0)
+	{
+		// Default Algorithm
+		cipher_algorithm = PGP_AES_128;
+		aead_algorithm = PGP_AEAD_OCB;
+	}
+	else
+	{
+		cipher_algorithm = algorithm % cipher_algorithm_count;
+		aead_algorithm = (algorithm / cipher_algorithm_count) + 1;
+	}
+
+	return ((cipher_algorithm << 8) + aead_algorithm);
+}
