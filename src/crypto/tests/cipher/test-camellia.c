e382c66112196cbb68f112b39135fe129a52bf33
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-05-26 23:47:00 +0530

    Update camellia tests

--- src/crypto/tests/cipher/test-camellia.c
+++ src/crypto/tests/cipher/test-camellia.c
@@ -20,20 +20,20 @@ int32_t camellia128_test_suite(void)
 	byte_t plaintext[CAMELLIA_BLOCK_SIZE];
 	byte_t ciphertext[CAMELLIA_BLOCK_SIZE];
 
+	camellia_key key = {0};
+
 	hex_to_block(secret, CAMELLIA128_KEY_SIZE, "0123456789abcdeffedcba9876543210");
 	hex_to_block(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
 
-	camellia_key *key = camellia_key_new(CAMELLIA128, secret, CAMELLIA128_KEY_SIZE);
+	camellia128_key_init(&key, secret);
 
 	memset(ciphertext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_encrypt_block(key, plaintext, ciphertext);
+	camellia128_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, CAMELLIA_BLOCK_SIZE, "67673138549669730857065648eabe43");
 
 	memset(plaintext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_decrypt_block(key, ciphertext, plaintext);
+	camellia128_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
-
-	camellia_key_delete(key);
 
 	return status;
 }
@@ -45,20 +45,20 @@ int32_t camellia192_test_suite(void)
 	byte_t plaintext[CAMELLIA_BLOCK_SIZE];
 	byte_t ciphertext[CAMELLIA_BLOCK_SIZE];
 
+	camellia_key key = {0};
+
 	hex_to_block(secret, CAMELLIA192_KEY_SIZE, "0123456789abcdeffedcba98765432100011223344556677");
 	hex_to_block(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
 
-	camellia_key *key = camellia_key_new(CAMELLIA192, secret, CAMELLIA192_KEY_SIZE);
+	camellia192_key_init(&key, secret);
 
 	memset(ciphertext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_encrypt_block(key, plaintext, ciphertext);
+	camellia192_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, CAMELLIA_BLOCK_SIZE, "b4993401b3e996f84ee5cee7d79b09b9");
 
 	memset(plaintext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_decrypt_block(key, ciphertext, plaintext);
+	camellia192_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
-
-	camellia_key_delete(key);
 
 	return status;
 }
@@ -70,20 +70,20 @@ int32_t camellia256_test_suite(void)
 	byte_t plaintext[CAMELLIA_BLOCK_SIZE];
 	byte_t ciphertext[CAMELLIA_BLOCK_SIZE];
 
+	camellia_key key = {0};
+
 	hex_to_block(secret, CAMELLIA256_KEY_SIZE, "0123456789abcdeffedcba987654321000112233445566778899aabbccddeeff");
 	hex_to_block(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
 
-	camellia_key *key = camellia_key_new(CAMELLIA256, secret, CAMELLIA256_KEY_SIZE);
+	camellia256_key_init(&key, secret);
 
 	memset(ciphertext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_encrypt_block(key, plaintext, ciphertext);
+	camellia256_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, CAMELLIA_BLOCK_SIZE, "9acc237dff16d76c20ef7c919e3a7509");
 
 	memset(plaintext, 0, CAMELLIA_BLOCK_SIZE);
-	camellia_decrypt_block(key, ciphertext, plaintext);
+	camellia256_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, CAMELLIA_BLOCK_SIZE, "0123456789abcdeffedcba9876543210");
-
-	camellia_key_delete(key);
 
 	return status;
 }
