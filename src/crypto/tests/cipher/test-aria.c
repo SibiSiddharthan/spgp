55cc6e6d9181e5b46c3231bdb8999e2f67cbbe89
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-05-26 23:42:10 +0530

    Update aria tests

--- src/crypto/tests/cipher/test-aria.c
+++ src/crypto/tests/cipher/test-aria.c
@@ -20,20 +20,20 @@ int32_t aria128_test_suite(void)
 	byte_t plaintext[ARIA_BLOCK_SIZE];
 	byte_t ciphertext[ARIA_BLOCK_SIZE];
 
+	aria_key key = {0};
+
 	hex_to_block(secret, ARIA128_KEY_SIZE, "000102030405060708090a0b0c0d0e0f");
 	hex_to_block(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
 
-	aria_key *key = aria_key_new(ARIA128, secret,ARIA128_KEY_SIZE);
+	aria128_key_init(&key, secret);
 
 	memset(ciphertext, 0, ARIA_BLOCK_SIZE);
-	aria_encrypt_block(key, plaintext, ciphertext);
+	aria128_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, ARIA_BLOCK_SIZE, "d718fbd6ab644c739da95f3be6451778");
 
 	memset(plaintext, 0, ARIA_BLOCK_SIZE);
-	aria_decrypt_block(key, ciphertext, plaintext);
+	aria128_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
-
-	aria_key_delete(key);
 
 	return status;
 }
@@ -45,20 +45,20 @@ int32_t aria192_test_suite(void)
 	byte_t plaintext[ARIA_BLOCK_SIZE];
 	byte_t ciphertext[ARIA_BLOCK_SIZE];
 
+	aria_key key = {0};
+
 	hex_to_block(secret, ARIA192_KEY_SIZE, "000102030405060708090a0b0c0d0e0f1011121314151617");
 	hex_to_block(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
 
-	aria_key *key = aria_key_new(ARIA192, secret, ARIA192_KEY_SIZE);
+	aria192_key_init(&key, secret);
 
 	memset(ciphertext, 0, ARIA_BLOCK_SIZE);
-	aria_encrypt_block(key, plaintext, ciphertext);
+	aria192_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, ARIA_BLOCK_SIZE, "26449c1805dbe7aa25a468ce263a9e79");
 
 	memset(plaintext, 0, ARIA_BLOCK_SIZE);
-	aria_decrypt_block(key, ciphertext, plaintext);
+	aria192_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
-
-	aria_key_delete(key);
 
 	return status;
 }
@@ -70,20 +70,20 @@ int32_t aria256_test_suite(void)
 	byte_t plaintext[ARIA_BLOCK_SIZE];
 	byte_t ciphertext[ARIA_BLOCK_SIZE];
 
+	aria_key key = {0};
+
 	hex_to_block(secret, ARIA256_KEY_SIZE, "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f");
 	hex_to_block(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
 
-	aria_key *key = aria_key_new(ARIA256, secret, ARIA256_KEY_SIZE);
+	aria256_key_init(&key, secret);
 
 	memset(ciphertext, 0, ARIA_BLOCK_SIZE);
-	aria_encrypt_block(key, plaintext, ciphertext);
+	aria256_encrypt_block(&key, plaintext, ciphertext);
 	status += CHECK_BLOCK(ciphertext, ARIA_BLOCK_SIZE, "f92bd7c79fb72e2f2b8f80c1972d24fc");
 
 	memset(plaintext, 0, ARIA_BLOCK_SIZE);
-	aria_decrypt_block(key, ciphertext, plaintext);
+	aria256_decrypt_block(&key, ciphertext, plaintext);
 	status += CHECK_BLOCK(plaintext, ARIA_BLOCK_SIZE, "00112233445566778899aabbccddeeff");
-
-	aria_key_delete(key);
 
 	return status;
 }
