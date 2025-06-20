97c5b5fab0a16f1147cf0a79d75f513b346a532a
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-06-20 17:51:15 +0530

    Fix nt.h for newer sdks

--- src/io/include/win32/nt.h
+++ src/io/include/win32/nt.h
@@ -608,21 +608,6 @@ typedef struct _FILE_END_OF_FILE_INFORMATION
 
 //================ FileStatInformation ========================================
 
-typedef struct _FILE_STAT_INFORMATION
-{
-	LARGE_INTEGER FileId;
-	LARGE_INTEGER CreationTime;
-	LARGE_INTEGER LastAccessTime;
-	LARGE_INTEGER LastWriteTime;
-	LARGE_INTEGER ChangeTime;
-	LARGE_INTEGER AllocationSize;
-	LARGE_INTEGER EndOfFile;
-	ULONG FileAttributes;
-	ULONG ReparseTag;
-	ULONG NumberOfLinks;
-	ACCESS_MASK EffectiveAccess;
-} FILE_STAT_INFORMATION, *PFILE_STAT_INFORMATION;
-
 typedef struct _FILE_POSITION_INFORMATION
 {
 	LARGE_INTEGER CurrentByteOffset;
