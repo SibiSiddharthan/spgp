10322594b14e59a431566eab4b2af75b9b95c2f6
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-06-29 21:13:40 +0530

    Move unused.h to core

---
+++ src/core/include/unused.h
@@ -0,0 +1,13 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef SPGP_UNUSED_H
+#define SPGP_UNUSED_H
+
+#define UNUSED(x) ((void)(x))
+
+#endif
