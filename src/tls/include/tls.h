45fe555568a832098be90081f4aef06285cf7d28
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-07-17 02:46:16 +0530

    Move the tls version defines to a separate header

--- src/tls/include/tls.h
+++ src/tls/include/tls.h
@@ -8,17 +8,10 @@
 #ifndef TLS_H
 #define TLS_H
 
-// Versions
-#define TLS_VERSION_1_0 0x0301 // TLS 1.0
-#define TLS_VERSION_1_1 0x0302 // TLS 1.1
-#define TLS_VERSION_1_2 0x0303 // TLS 1.2
-#define TLS_VERSION_1_3 0x0304 // TLS 1.3
-
-#define TLS_VERSION_DEFAULT TLS_VERSION_1_2
-
 #include <tls/alert.h>
 #include <tls/algorithms.h>
 #include <tls/handshake.h>
 #include <tls/record.h>
+#include <tls/version.h>
 
 #endif
---
+++ src/tls/include/tls/version.h
@@ -0,0 +1,18 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef TLS_VERSION_H
+#define TLS_VERSION_H
+
+#define TLS_VERSION_1_0 0x0301 // TLS 1.0
+#define TLS_VERSION_1_1 0x0302 // TLS 1.1
+#define TLS_VERSION_1_2 0x0303 // TLS 1.2
+#define TLS_VERSION_1_3 0x0304 // TLS 1.3
+
+#define TLS_VERSION_DEFAULT TLS_VERSION_1_2
+
+#endif
