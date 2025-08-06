b08728c2c0062287e0ae6a4c4d5dc433dec103c2
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-07 01:39:24 +0530

    Add dtls version identifiers

--- src/tls/include/tls/version.h
+++ src/tls/include/tls/version.h
@@ -17,6 +17,12 @@
 
 #define TLS_VERSION_DEFAULT TLS_VERSION_1_2
 
+#define DLS_VERSION_1_0 0xFEFF // DTLS 1.0
+#define DLS_VERSION_1_2 0xFEFD // DTLS 1.2
+#define DLS_VERSION_1_3 0xFEFC // DTLS 1.3
+
+#define DTLS_VERSION_DEFAULT DTLS_VERSION_1_2
+
 typedef struct _tls_protocol_version
 {
 	uint8_t major;
