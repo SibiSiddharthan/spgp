4d9b9beed81a64053cef410cd7cdd185ed89dba4
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-19 23:52:36 +0530

    Initial argument count

--- src/common/src/print.c
+++ src/common/src/print.c
@@ -8,6 +8,49 @@
 #include <print.h>
 #include <buffer.h>
 
+#include <stdlib.h>
+
+static uint32_t parse_print_specifier(const char *format)
+{
+}
+
+static uint32_t count_print_args(const char *format, va_list args)
+{
+	byte_t byte = 0;
+	uint32_t count = 0;
+
+	void **arg = NULL;
+
+	while ((byte = *format++) != '\0')
+	{
+		if (byte == '%')
+		{
+			byte = *format++;
+
+			if (byte == '\0')
+			{
+				break;
+			}
+
+			if (byte == '%')
+			{
+				continue;
+			}
+
+			count += 1;
+		}
+	}
+
+	arg = malloc(count * sizeof(void *));
+
+	for (uint32_t i = 0; i < count; ++i)
+	{
+		arg[i] = va_arg(args, void *);
+	}
+
+	return count;
+}
+
 uint32_t vxprint(buffer_t *buffer, const char *format, va_list args)
 {
 	uint32_t result = 0;
@@ -28,6 +71,15 @@ uint32_t vxprint(buffer_t *buffer, const char *format, va_list args)
 			{
 				result += writebyte(buffer, '%');
 				continue;
+			}
+
+			while ((byte = *format++) != '\0')
+			{
+
+				// ll and hh
+				if (byte == 'l' || byte == 'h')
+				{
+				}
 			}
 
 			continue;
