9cc5a21adca4d69318df68bbd83f8513a162f3a0
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-19 23:37:49 +0530

    Initial print implementation

--- src/common/src/print.c
+++ src/common/src/print.c
@@ -8,3 +8,33 @@
 #include <print.h>
 #include <buffer.h>
 
+uint32_t vxprint(buffer_t *buffer, const char *format, va_list args)
+{
+	uint32_t result = 0;
+	byte_t byte = 0;
+
+	while ((byte = *format++) != '\0')
+	{
+		if (byte == '%')
+		{
+			byte = *format++;
+
+			if (byte == '\0')
+			{
+				break;
+			}
+
+			if (byte == '%')
+			{
+				result += writebyte(buffer, '%');
+				continue;
+			}
+
+			continue;
+		}
+
+		result += writebyte(buffer, byte);
+	}
+
+	return result;
+}
