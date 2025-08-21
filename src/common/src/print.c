6e2e48db4ce886765bf04a2b6d27d6b18abc7a3b
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-21 16:36:51 +0530

    Remove the argument counting function

--- src/common/src/print.c
+++ src/common/src/print.c
@@ -136,43 +136,6 @@ static uint32_t parse_print_specifier(const char *format)
 {
 }
 
-static uint32_t count_print_args(const char *format, va_list args)
-{
-	byte_t byte = 0;
-	uint32_t count = 0;
-
-	void **arg = NULL;
-
-	while ((byte = *format++) != '\0')
-	{
-		if (byte == '%')
-		{
-			byte = *format++;
-
-			if (byte == '\0')
-			{
-				break;
-			}
-
-			if (byte == '%')
-			{
-				continue;
-			}
-
-			count += 1;
-		}
-	}
-
-	arg = malloc(count * sizeof(void *));
-
-	for (uint32_t i = 0; i < count; ++i)
-	{
-		arg[i] = va_arg(args, void *);
-	}
-
-	return count;
-}
-
 uint32_t vxprint(buffer_t *buffer, const char *format, va_list args)
 {
 	uint32_t result = 0;
