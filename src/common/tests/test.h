1fc20cba1db0b363dad54dca83713bc3169d07f9
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-03 03:59:25 +0530

    Add basic string testing

---
+++ src/common/tests/test.h
@@ -0,0 +1,28 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef COMMON_TEST_H
+#define COMMON_TEST_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+uint32_t check_value_string(const char *actual, const char *expected, const char *expression, const char *function, int32_t line)
+{
+	if (strcmp(actual, expected) != 0)
+	{
+		printf("Value does not match in %s:%d.\n(%s) -> (%s == %s)\n", function, line, expression, actual, expected);
+		return 1;
+	}
+
+	return 0;
+}
+
+#define CHECK_STRING(ACTUAL, EXPECT) check_value_string(ACTUAL, EXPECT, #ACTUAL " == " #EXPECT, __FUNCTION__, __LINE__)
+
+#endif
