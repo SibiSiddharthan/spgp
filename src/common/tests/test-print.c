72991365022185e1cf599845f91a4d1310410fe2
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-03 04:00:17 +0530

    Add dummy tests for print

---
+++ src/common/tests/test-print.c
@@ -0,0 +1,12 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include "test.h"
+
+int main()
+{
+}
