adab8b01a38d6b2f796724326d2b66217d4460d5
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-04 16:07:00 +0530

    Add simple tests for print

--- src/common/tests/test-print.c
+++ src/common/tests/test-print.c
@@ -5,8 +5,24 @@
    Refer to the LICENSE file at the root directory for details.
 */
 
+#include <print.h>
 #include "test.h"
+
+uint32_t test_simple(void)
+{
+	uint32_t status = 0;
+
+	uint32_t result = 0;
+	char buffer[256] = {0};
+
+	result = sprint(buffer, 256, "abcd");
+
+	status += CHECK_STRING(buffer, "abcd");
+
+	return status;
+}
 
 int main()
 {
+	return test_simple();
 }
