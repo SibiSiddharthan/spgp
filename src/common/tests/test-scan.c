2d30322b3af0a44d412204ace58b6d3c4460c593
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-20 03:25:51 +0530

    Prepare scan test

---
+++ src/common/tests/test-scan.c
@@ -0,0 +1,15 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#include <scan.h>
+#include <string.h>
+#include "test.h"
+
+int main()
+{
+	return 0;
+}
