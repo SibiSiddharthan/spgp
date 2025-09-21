f824048e2f69650b7b3565f9a70c29a572c0ceb0
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-21 14:29:30 +0530

    Add scan test

--- src/common/tests/test-scan.c
+++ src/common/tests/test-scan.c
@@ -23,32 +23,32 @@ uint32_t test_simple(void)
 	status += CHECK_RESULT(result, 0);
 
 	result = sscan("a", 0, "%c", &c1);
-	status += CHECK_RESULT(c1, 'a');
+	status += CHECK_UVALUE(c1, 'a');
 	status += CHECK_RESULT(result, 1);
 
 	result = sscan("bc", 0, "%c%c", &c1, &c2);
-	status += CHECK_RESULT(c1, 'b');
-	status += CHECK_RESULT(c2, 'c');
+	status += CHECK_UVALUE(c1, 'b');
+	status += CHECK_UVALUE(c2, 'c');
 	status += CHECK_RESULT(result, 2);
 
 	result = sscan("de", 0, "%c  %c", &c1, &c2);
-	status += CHECK_RESULT(c1, 'd');
-	status += CHECK_RESULT(c2, 'e');
+	status += CHECK_UVALUE(c1, 'd');
+	status += CHECK_UVALUE(c2, 'e');
 	status += CHECK_RESULT(result, 2);
 
 	result = sscan("f  g", 0, "%c%c", &c1, &c2);
-	status += CHECK_RESULT(c1, 'f');
-	status += CHECK_RESULT(c2, 'g');
+	status += CHECK_UVALUE(c1, 'f');
+	status += CHECK_UVALUE(c2, 'g');
 	status += CHECK_RESULT(result, 2);
 
 	result = sscan("h  i", 0, "%c %c", &c1, &c2);
-	status += CHECK_RESULT(c1, 'h');
-	status += CHECK_RESULT(c2, 'i');
+	status += CHECK_UVALUE(c1, 'h');
+	status += CHECK_UVALUE(c2, 'i');
 	status += CHECK_RESULT(result, 2);
 
 	result = sscan("jh   gk", 0, "%ch g%c", &c1, &c2);
-	status += CHECK_RESULT(c1, 'j');
-	status += CHECK_RESULT(c2, 'k');
+	status += CHECK_UVALUE(c1, 'j');
+	status += CHECK_UVALUE(c2, 'k');
 	status += CHECK_RESULT(result, 2);
 
 	return status;
