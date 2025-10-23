6763b62570b451a0b7bcf6ccf66fb132c743775c
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-10-24 00:10:59 +0530

    Update tls alert printing

--- src/tls/src/alert.c
+++ src/tls/src/alert.c
@@ -65,131 +65,133 @@ uint32_t tls_alert_write_body(tls_alert *alert, void *buffer, uint32_t size)
 	return pos;
 }
 
-uint32_t tls_alert_print_body(tls_alert *alert, void *buffer, uint32_t size, uint32_t indent)
+uint32_t tls_alert_print_body(tls_alert *alert, buffer_t *buffer, uint32_t indent)
 {
 	uint32_t pos = 0;
+	const char *level_name = NULL;
+	const char *description_name = NULL;
 
 	// Alert Level
-	pos += print_format(indent, PTR_OFFSET(buffer, pos), size - pos, "Level: ");
-
 	switch (alert->level)
 	{
 	case TLS_WARNING:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Warning (ID 1)\n");
+		level_name = "Warning";
 		break;
 	case TLS_FATAL:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Fatal (ID 2)\n");
+		level_name = "Fatal";
 		break;
 	default:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unknown (ID %hhu)\n", alert->level);
+		level_name = "Unknown";
 		break;
 	}
 
-	// Alert Description
-	pos += print_format(indent, PTR_OFFSET(buffer, pos), size - pos, "Description: ");
+	pos += print_format(buffer, indent, "Level: %s (ID %hhu)\n", level_name, alert->level);
 
+	// Alert Description
 	switch (alert->description)
 	{
 	case TLS_CLOSE_NOTIFY:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Close Notification (ID 0)\n");
+		description_name = "Close Notification";
 		break;
 	case TLS_UNEXPECTED_MESSAGE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unexpected Message (ID 10)\n");
+		description_name = "Unexpected Message";
 		break;
 	case TLS_BAD_RECORD_MAC:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Bad Record MAC (ID 20)\n");
+		description_name = "Bad Record MAC";
 		break;
 	case TLS_DECRYPTION_FAILURE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Decryption Failure (ID 21)\n");
+		description_name = "Decryption Failure";
 		break;
 	case TLS_RECORD_OVERFLOW:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Record Overflow (ID 22)\n");
+		description_name = "Record Overflow";
 		break;
 	case TLS_DECOMPRESSION_FAILURE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Decompression Failure (ID 30)\n");
+		description_name = "Decompression Failure";
 		break;
 	case TLS_HANDSHAKE_FAILURE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Handshake Failure (ID 40)\n");
+		description_name = "Handshake Failure";
 		break;
 	case TLS_NO_CERTIFICATE_RESERVED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "No Certificate Reserved (ID 41)\n");
+		description_name = "No Certificate Reserved";
 		break;
 	case TLS_BAD_CERTIFICATE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Bad Certificate (ID 42)\n");
+		description_name = "Bad Certificate";
 		break;
 	case TLS_UNSUPPORTED_CERTIFICATE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unsupported Certificate (ID 43)\n");
+		description_name = "Unsupported Certificate";
 		break;
 	case TLS_CERTIFICATE_REVOKED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Certificate Revoked (ID 44)\n");
+		description_name = "Certificate Revoked";
 		break;
 	case TLS_CERTIFICATE_EXPIRED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Certificate Expired (ID 45)\n");
+		description_name = "Certificate Expired";
 		break;
 	case TLS_CERTIFICATE_UNKNOWN:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Certificate Unknown (ID 46)\n");
+		description_name = "Certificate Unknown";
 		break;
 	case TLS_ILLEGAL_PARAMETER:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Illegal Parameter (ID 47)\n");
+		description_name = "Illegal Parameter";
 		break;
 	case TLS_UNKNOWN_CA:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unknown Certificate Authority (ID 48)\n");
+		description_name = "Unknown Certificate Authority";
 		break;
 	case TLS_ACCESS_DENIED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Access Denied (ID 49)\n");
+		description_name = "Access Denied";
 		break;
 	case TLS_DECODE_ERROR:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Decode Error (ID 50)\n");
+		description_name = "Decode Error";
 		break;
 	case TLS_DECRYPT_ERROR:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Decrypt Error (ID 51)\n");
+		description_name = "Decrypt Error";
 		break;
 	case TLS_EXPORT_RESTRICTION:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Export Restriction (ID 60)\n");
+		description_name = "Export Restriction";
 		break;
 	case TLS_PROTOCOL_VERSION:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Protocol Version (ID 70)\n");
+		description_name = "Protocol Version";
 		break;
 	case TLS_INSUFFICIENT_SECURITY:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Insufficient Security (ID 71)\n");
+		description_name = "Insufficient Security";
 		break;
 	case TLS_INTERNAL_ERROR:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Internal Error (ID 80)\n");
+		description_name = "Internal Error";
 		break;
 	case TLS_INAPPROPRIATE_FALLBACK:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Inappropriate Fallback (ID 86)\n");
+		description_name = "Inappropriate Fallback";
 		break;
 	case TLS_USER_CANCELLED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "User Cancelled (ID 90)\n");
+		description_name = "User Cancelled";
 		break;
 	case TLS_NO_RENEGOTIATION:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "No Renegotiation (ID 100)\n");
+		description_name = "No Renegotiation";
 		break;
 	case TLS_MISSING_EXTENSION:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Missing Extension (ID 109)\n");
+		description_name = "Missing Extension";
 		break;
 	case TLS_UNSUPPORTED_EXTENSION:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unsupported Extension (ID 110)\n");
+		description_name = "Unsupported Extension";
 		break;
 	case TLS_UNRECOGNIZED_NAME:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unrecognized Name (ID 112)\n");
+		description_name = "Unrecognized Name";
 		break;
 	case TLS_BAD_CERTIFICATE_STATUS_RESPONSE:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Bad Certificate Status Response (ID 113)\n");
+		description_name = "Bad Certificate Status Response";
 		break;
 	case TLS_UNKNOWN_PSK_IDENTITY:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unknown Pre-Shared Key Identity (ID 115)\n");
+		description_name = "Unknown Pre-Shared Key Identity";
 		break;
 	case TLS_CERTIFICATE_REQUIRED:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Certificate Required (ID 116)\n");
+		description_name = "Certificate Required";
 		break;
 	case TLS_NO_APPLICATION_PROTOCOL:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "No Application Protocol (ID 120)\n");
+		description_name = "No Application Protocol";
 		break;
 	default:
-		pos += snprintf(PTR_OFFSET(buffer, pos), size - pos, "Unknown (ID %hhu)\n", alert->description);
+		description_name = "Unknown";
 		break;
 	}
+
+	pos += print_format(buffer, indent, "Description: %s (ID %hhu)\n", description_name, alert->description);
 
 	return pos;
 }
