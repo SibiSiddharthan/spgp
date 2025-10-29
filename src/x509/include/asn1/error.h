2fcb38b0f7d386ff2a3f723094051874342e6e5a
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-10-29 15:21:51 +0530

    Add asn1 error enum

---
+++ src/x509/include/asn1/error.h
@@ -0,0 +1,16 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef ASN1_ERROR_H
+#define ASN1_ERROR_H
+
+typedef enum _asn1_error_t
+{
+	ASN1_FIELD_LENGTH_TOO_BIG = 1
+} asn1_error_t;
+
+#endif
