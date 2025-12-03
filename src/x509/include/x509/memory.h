29a4c363a20f70afc36ffc00308b1e6a351ae383
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-12-04 00:42:30 +0530

    Copy the zmalloc of tls to x509 as well

---
+++ src/x509/include/x509/memory.h
@@ -0,0 +1,46 @@
+/*
+   Copyright (c) 2024 - 2025 Sibi Siddharthan
+
+   Distributed under the MIT license.
+   Refer to the LICENSE file at the root directory for details.
+*/
+
+#ifndef X509_MEMORY_H
+#define X509_MEMORY_H
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+static inline void *zmalloc(size_t size)
+{
+	void *mem = NULL;
+
+	mem = malloc(size);
+
+	if (mem == NULL)
+	{
+		return NULL;
+	}
+
+	memset(mem, 0, size);
+
+	return mem;
+}
+
+static inline void *zrealloc(void *old, size_t size)
+{
+	void *mem = NULL;
+
+	mem = realloc(old, size);
+
+	if (mem == NULL)
+	{
+		free(old);
+		return NULL;
+	}
+
+	return mem;
+}
+
+#endif
